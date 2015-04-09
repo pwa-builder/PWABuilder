e9fd0dd6133211e8f70e505146fc9863570c26f9
Alex Porter <alex.porter@bamideas.com>
2015-04-09 10:51:38 -0400

    Adding Ember, initial prototype buildout

---
+++ app/router.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+export default Router.map(function() {
+	this.route('build'); //Build page
+	this.route('usage'); //Usage page
+});
