0c5187eebc22db0067410ab37ec22de8eec30d1a
Alex Porter <alex.porter@bamideas.com>
2015-04-16 15:26:11 -0400

    First pass at homepage structure complete

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/prism.css');
+app.import('vendor/prism.js');
+
 module.exports = app.toTree();
