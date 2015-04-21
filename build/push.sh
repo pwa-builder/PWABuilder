cbd72e55fbab5ad5ad8f4fba90ca50d9c00da9ec
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-04-21 17:18:53 -0400

    adding web config for svg handling, back to old way of building

--- build/push.sh
+++ build/push.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+rm -rf .git
+ember build -prod
+cd ./dist
+git init
+git config user.email "interactive@bamideas.com"
+git config user.name "BaM Interactive"
 git remote add azure "https://baminteractive:$STAGING_PASS@manifold-site-staging.scm.azurewebsites.net:443/manifold-site-staging.git" || true
 git checkout -b deploy
+git add -A
+git commit -am "Deployment $(date +%s)"
 git push --force azure deploy:master
