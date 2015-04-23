21c0bfac3ba0448c241e3b7779840192bde94c77
Nathan Bingham <nsbingham@gmail.com>
2015-04-22 20:18:25 -0400

    cleaning up unused tests

--- app/components/manifest-upload.js
+++ app/components/manifest-upload.js
@@ -1,20 +1,20 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
+import config from '../config/environment';
 
 export default Ember.Component.extend({
-  generateFormData: function(file) {
-  
+  generateFormData: function() {
+
   },
   uploadManifest: function() {
     ajax({
-      url:config.APP.API_URL+'/manifest',
+      url: config.APP.API_URL + '/manifest',
         type: 'POST',
-        data: JSON.stringify({ image: { src: self.logoUrl }}),
+        data: null,
         dataType: 'json',
         contentType: 'application/json; charset=utf-8'
       }).then(function(result) {
-        self.logos.pushObject({ src: self.logoUrl, sizes: result.meta.width +'x'+result.meta.height });
-        self.set('logoUrl','');
-        self.sendAction('action',self.logos);
+        console.log(result);
       });
 
   }
