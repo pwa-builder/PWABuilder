3bfc443867b7d042b3d9b99e2176d5fe0d3fadee
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-04-28 12:15:27 -0400

    Testing out basic auth and prod push

--- build/push_prod.sh
+++ build/push_prod.sh
@@ -7,7 +7,7 @@ cd ./dist
 git init
 git config user.email "interactive@bamideas.com"
 git config user.name "BaM Interactive"
-git remote add azure "https://baminteractive:$STAGING_PASS@manifold-site.scm.azurewebsites.net:443/manifold-site.git" || true
+git remote add azure "https://baminteractive:$STAGING_PASS@manifold-site-prod.scm.azurewebsites.net:443/manifold-site-prod.git" || true
 git checkout -b deploy
 git add -A
 git commit -am "Deployment $(date +%s)"
