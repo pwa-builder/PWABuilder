5f6c6c4998a807b6d1f9db160c0780582ffddabc
Nathan Bingham <nsbingham@gmail.com>
2015-04-28 21:54:04 -0400

    Disabling auto-loading of assets

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,9 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  emberCliFontAwesome: { includeFontAwesomeAssets: false }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
