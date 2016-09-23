00663e792a33de2cb25f27958ad265e365df13e2
Marcelo Rodriguez <marcelo.rodriguez@southworks.net>
2016-09-23 17:18:36 -0300

    Changed API URL port for development env

--- config/environment.js
+++ config/environment.js
@@ -31,7 +31,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.APP.API_URL='http://localhost:4200';
+    ENV.APP.API_URL='http://localhost:3000';
   }
 
   if (environment === 'test') {
