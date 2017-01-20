572fa3b11b2639059519f0452abebe1f3fda0403
Jeff Burtoft <boyofgreen@gmail.com>
2017-01-19 19:12:21 -0800

    update look and feel

--- app/helpers/imagegenerator-link.js
+++ app/helpers/imagegenerator-link.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.HTMLBars.makeBoundHelper(function(){
-    return new Ember.Handlebars.SafeString('<a href=\''+config.APP.IMAGEGENERATOR_URL+'\'>App Image Generator</a>');
+    return new Ember.Handlebars.SafeString('<a href=\''+config.APP.IMAGEGENERATOR_URL+'\'>PWA Image Generator</a>');
 });
