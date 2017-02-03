4069a53bf0dad5b36d71b0e27043a7e1e2550631
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-02-03 12:41:56 +0100

    Add header

--- app/helpers/imagegenerator-link.js
+++ app/helpers/imagegenerator-link.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.HTMLBars.makeBoundHelper(function(){
-    return new Ember.Handlebars.SafeString('<a href=\''+config.APP.IMAGEGENERATOR_URL+'\'>PWA Image Generator</a>');
+    return new Ember.Handlebars.SafeString('<a class="pwa-header-link" href=\''+config.APP.IMAGEGENERATOR_URL+'\'>PWA Image Generator</a>');
 });
