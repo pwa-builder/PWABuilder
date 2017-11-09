b4e3fbad4f3ea48cbb798b3c1fd8cf52da4488ea
Jeff Burtoft <boyofgreen@gmail.com>
2017-11-09 14:21:56 -0600

    update for nonrelative URLs

--- app/helpers/icon-display.js
+++ app/helpers/icon-display.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.HTMLBars.makeBoundHelper(function(value, options) {
   var escapedSrc = Ember.Handlebars.Utils.escapeExpression(options.src),
     escapedBase = Ember.Handlebars.Utils.escapeExpression(options.base);
+  /*adding in this fix for URLs from wrong path, but need to make sure it works with mastodon.social for rebuild */
+    var pathArray = escapedBase.split( '/' );
+    var protocol = pathArray[0];
+    var host = pathArray[2];
+    escapedBase = protocol + '//' + host;
 
     if(escapedSrc.charAt(0) === '/'){
       escapedSrc = escapedSrc.substring(1, escapedSrc.length);
