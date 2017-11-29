0869e2356bb87735e9d715d6a4e395e495f0c5c6
Quique Fdez Guerra <quique@ckgrafico.com>
2017-11-29 18:46:28 +0100

    Add SCSS to generic assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
   /*
   ** Build configuration
   */
-  css: ['tachyons/css/tachyons.min.css', '~/assets/css/main.css'],
+  css: ['tachyons/css/tachyons.min.css', '~/assets/scss/main.scss'],
   build: {
     vendor: ['axios', 'gsap', 'vuex-class', 'nuxt-class-component']
   },
