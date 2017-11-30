dbe2cd16164aa5b1c7aa36cbfc50e57658c07928
Quique Fdez Guerra <quique@ckgrafico.com>
2017-11-30 10:54:22 +0100

    Finish basic project configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,18 @@ module.exports = {
     baseUrl: process.env.BASE_URL || 'http://localhost:3000'
   },
   head: {
-    title: 'starter',
+    title: 'PWABuilder',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'All the tools you need to build and deploy your Progressive Web Apps.' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-  loading: { color: '#3B8070' },
+  loading: { color: '#1FC2C8' },
   css: ['tachyons/css/tachyons.min.css', '~/assets/scss/main.scss'],
   build: {
+    extractCSS: true,
     vendor: ['axios', 'gsap', 'vuex-class', 'nuxt-class-component']
   },
   modules: ['~/modules/typescript']
