dd282aa6ef9355f55657ffb90093aceb4bceae4a
Quique Fdez Guerra <quique@ckgrafico.com>
2017-11-30 13:12:17 +0100

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- Old header -->
     <header class="pwa-header pure-g">
       <div class="pwa-header-left pure-u pure-u-md-1-5">
         <nuxt-link to="/">
@@ -29,5 +28,33 @@
     <div class="container">
       <nuxt/>
     </div>
+
+    <footer class="pwa-footer pure-g is-small">
+      <div class="pure-u-1-2 pure-u-md-1-5">
+        <a id="ember388" class="ember-view pwa-footer-action pwa-footer-action--block active" href="/generator">Generate</a>
+        <a class="pwa-footer-action pwa-footer-action--block" href="http://docs.pwabuilder.com/" target="_blank">Documentation</a>
+      </div>
+      <div class="pure-u-1-2 pure-u-md-1-5">
+        <a class="pwa-footer-action pwa-footer-action--block" href="//appimagegenerator-prod.azurewebsites.net">PWA Image Generator</a>
+        <a class="pwa-footer-action pwa-footer-action--block" href="https://github.com/pwa-builder" target="_blank">Github</a>
+      </div>
+      <div class="pwa-footer-social pure-u-1 pure-u-md-3-5">
+        <a class="pwa-footer-logo_action" href="https://twitter.com/boyofgreen" target="_blank">
+          <img class="pwa-footer-logo" src="~/assets/images/logo_twitter.svg">
+        </a>
+        <a class="pwa-footer-logo_action" href="https://github.com/pwa-builder" target="_blank">
+          <img class="pwa-footer-logo" src="~/assets/images/logo_github.svg">
+        </a>
+        <a class="pwa-footer-logo_action" href="https://www.npmjs.com/package/manifoldjs" target="_blank">
+          <img class="pwa-footer-logo pwa-footer-logo--big" src="~/assets/images/logo_npm.svg">
+        </a>
+        <a class="pwa-footer-logo_action" href="http://stackoverflow.com/questions/tagged/manifoldjs" target="_blank">
+          <img class="pwa-footer-logo pwa-footer-logo--small" src="~/assets/images/logo_stackoverflow.svg">
+        </a>
+      </div>
+      <div class="pure-u-1 pwa-footer-copy">
+        PWABuilder was founded by Microsoft as a community guided, open source project to help move PWA adoption forward. <span class="divider">|</span> <a href="https://github.com/pwa-builder/ManifoldJS#license" target="_blank" class="pwa-footer-action">License</a>
+      </div>
+    </footer>
   </div>
 </template>
