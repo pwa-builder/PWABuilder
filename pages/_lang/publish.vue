ec0654d352d98e11652aef07d0ee1818848c7ce8
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-12-04 13:55:31 +0100

    Import store using alias

--- pages/_lang/publish.vue
+++ pages/_lang/publish.vue
@@ -11,7 +11,7 @@ import Vue from 'vue';
 import Component from 'nuxt-class-component';
 import { Action, State, namespace } from 'vuex-class';
 
-import { modules } from '~/store';
+import { modules } from 'store';
 import GeneratorMenu from '~/components/GeneratorMenu';
 import TwoWays from '~/components/TwoWays';
 
