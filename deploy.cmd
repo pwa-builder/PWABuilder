a21953a8faf08a18cac93c8979753f117b7c692c
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-12-05 12:29:07 +0100

    Add typescript to package.json Change to static deploy

--- deploy.cmd
+++ deploy.cmd
@@ -108,7 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Run prod script
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! run build
+  call :ExecuteCmd !NPM_CMD! run generate -- --spa
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
