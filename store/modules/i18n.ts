837256851392fc61ee5b40c01a269ffc6e38b9bd
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-12-11 19:22:32 +0100

    Add actions interface to i18n

--- store/modules/i18n.ts
+++ store/modules/i18n.ts
@@ -19,7 +19,9 @@ export const state = (): State => ({
 
 export const getters: GetterTree<State, RootState> = {};
 
-export const actions: ActionTree<State, RootState> = {};
+export interface Actions<S, R> extends ActionTree<S, R> {}
+
+export const actions: Actions<State, RootState> = {};
 
 export const mutations: MutationTree<State> = {
     [types.SET](state, locale) {
