7b1ba8600ce72f754d34ddb1f1501aa0e3101212
Oleksandr Kryzhanovskyy <okryz@plainconcepts.com>
2017-12-14 17:59:21 +0100

    Add publish page markup and translations

--- pages/_lang/publish.vue
+++ pages/_lang/publish.vue
@@ -1,14 +1,52 @@
 <template>
   <section>
     <GeneratorMenu/>
+    <div v-if="status">
+      <div class="pwa-infobox pwa-infobox--transparent l-pad l-pad--thin">
+        <div class="pure-g">
+          <div class="pure-u-1">
+            <h2 class="pwa-infobox-title pwa-infobox-title--centered">{{ $t('publish.title') }}</h2>
+          </div>
 
+          <div class="pure-u-1 pure-u-md-1-2">
+            <div class="pwa-infobox-box pwa-infobox-box--flat">
+              <h4 class="pwa-infobox-subtitle pwa-infobox-subtitle--thin">{{ $t('publish.web') }}</h4>
+              <p class="l-generator-description l-generator-description--fixed">
+                {{ $t('publish.web_description') }}
+              </p>
+              <span class="button-holder download-archive"><button data-flare="{'category': 'Download', 'action': 'Web', 'label': 'Download Archive', 'value': { 'page': '/download/web' }}" class="pwa-button pwa-button--simple pwa-button--brand">{{ $t('publish.download') }}</button></span>
+            </div>
+          </div>
 
-    <div class="step" v-if="status">
-      <div class="l-generator-buttons l-generator-buttons--centered">
-          <button @click="reset" class="pwa-button pwa-button--simple">{{ $t('publish.start_over') }}</button>
+          <div class="pure-u-1 pure-u-md-1-2">
+            <div class="pwa-infobox-box pwa-infobox-box--flat">
+              <h4 class="pwa-infobox-subtitle pwa-infobox-subtitle--thin">{{ $t('publish.windows') }}</h4>
+              <p class="l-generator-description l-generator-description--fixed">{{ $t('publish.windows_description') }}</p>
+              <span class="button-holder download-archive"><button data-flare="{'category': 'Download', 'action': 'Web', 'label': 'Download Archive', 'value': { 'page': '/download/web' }}" class="pwa-button pwa-button--simple  isEnabled">{{ $t('publish.download') }}</button></span>
+              <p><button class="pwa-button pwa-button--simple pwa-button--brand">{{ $t('publish.generate_appx') }}</button></p>
+            </div>
+          </div>
+          <div class="pure-u-1">
+            <div class="pwa-infobox-box pwa-infobox-box--flat">
+              <div class="pwa-infobox-padded">
+                <h4 class="pwa-infobox-subtitle pwa-infobox-subtitle--thin">{{ $t('publish.android') }}</h4>
+                <p class="l-generator-description l-generator-description--fixed l-generator-description--context">{{ $t('publish.android_description') }}</p>
+                <div>
+                  <button data-flare="{'category': 'Download', 'action': 'Web', 'label': 'Download Archive', 'value': { 'page': '/download/web' }}" class="pwa-button pwa-button--simple  isEnabled">{{ $t('publish.download') }}</button>
+                </div>
+              </div>
+              <h2 class="pwa-infobox-subtitle pwa-infobox-subtitle--thin">{{ $t('publish.polyfill') }}l</h2>
+              <button data-flare="{'category': 'Download', 'action': 'Web', 'label': 'Download Archive', 'value': { 'page': '/download/web' }}" class="pwa-button pwa-button--simple">{{ $t('publish.download') }}</button></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="step">
+        <div class="l-generator-buttons l-generator-buttons--centered">
+            <button @click="reset" class="pwa-button pwa-button--simple">{{ $t('publish.start_over') }}</button>
+        </div>
       </div>
     </div>
-
 
     <div class="l-generator-buttons l-generator-buttons--centered" v-if="!status">
       <p class="instructions">{{ $t('publish.manifest_needed') }}</p>
