10b9b20b2b18a63bc5e9b544c72642c17abf3264
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-12-28 14:00:20 +0100

    Improve i18n middleware

--- middleware/i18n.ts
+++ middleware/i18n.ts
@@ -1,4 +1,6 @@
 // From: https://nuxtjs.org/examples/i18n
+import * as i18n from '~/store/modules/i18n';
+
 export default ({ isHMR, app, store, route, params, error, redirect }) => {
     // If middleware is called from hot module replacement, ignore it
     if (isHMR) return;
@@ -8,7 +10,7 @@ export default ({ isHMR, app, store, route, params, error, redirect }) => {
       return error({ message: 'This page could not be found.', statusCode: 404 });
     }
     // Set locale
-    store.commit('i18n/SET', locale);
+    store.commit(`${i18n.name}/${i18n.types.SET}`, locale);
     app.i18n.locale = store.state.i18n.locale;
     // If route is /en/... -> redirect to /...
     if (locale === 'en' && route.fullPath.indexOf('/en') === 0) {
