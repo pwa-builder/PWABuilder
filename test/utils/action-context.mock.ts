96b0cda1cdc5977020e0fab21299b9ed46a83d2d
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-01-02 11:14:39 +0100

    Fix tests

--- test/utils/action-context.mock.ts
+++ test/utils/action-context.mock.ts
@@ -1,11 +1,18 @@
 import { ActionContext } from 'vuex';
 import { RootState, modules, state } from 'store';
 
+const modulesStates = {
+    generator: modules.generator.state(),
+    serviceworker: modules.serviceworker.state(),
+    publish: modules.publish.state(),
+    i18n: modules.i18n.state()
+};
+
 export let actionContextMockBuilder = <S>(s: S): ActionContext<S, RootState> => ({
     dispatch: (type: string) => Promise.resolve(),
     commit: (type: string) => {},
     state: s,
     getters: {},
-    rootState: {...modules, ...state()},
+    rootState: {...modulesStates, ...state()},
     rootGetters: {}
 });
