1d0788f71953f3da6ed6b694da6707d261550641
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-01-02 18:17:53 +0100

    Remove force from deployment command

--- deploy.cmd
+++ deploy.cmd
@@ -100,7 +100,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install -f
+  call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
