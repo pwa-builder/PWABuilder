9c47c9f09b76463a058284c6fd54c3b09fbeb711
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-01-03 17:17:26 +0100

    Refactor code structure RelatedApplications

--- components/RelatedApplications.vue
+++ components/RelatedApplications.vue
@@ -1,64 +1,62 @@
 <template>
 <section>
-    <div class="l-generator-field">
-        <label class="l-generator-label">{{ $t("related_applications.platform") }}
-            <a class="l-generator-link" href="https://www.w3.org/TR/appmanifest/#platform-member" target="_blank">[?]</a>
-        </label>
-        <input class="l-generator-input" v-model="platform" type="text">
+  <div class="l-generator-field">
+    <label class="l-generator-label">{{ $t("related_applications.platform") }}
+      <a class="l-generator-link" href="https://www.w3.org/TR/appmanifest/#platform-member" target="_blank">[?]</a>
+    </label>
+    <input class="l-generator-input" v-model="platform" type="text">
+  </div>
+  <div class="l-generator-field">
+    <label class="l-generator-label">{{ $t("related_applications.url") }}
+      <a class="l-generator-link" href="https://www.w3.org/TR/appmanifest/#url-member" target="_blank">[?]</a>
+    </label>
+    <input class="l-generator-input" v-model="url" type="text">
+
+  </div>
+  <div class="l-generator-field">
+    <label class="l-generator-label">{{ $t("related_applications.id") }}
+      <a class="l-generator-link" href="https://www.w3.org/TR/appmanifest/#id-member" target="_blank">[?]</a>
+    </label>
+    <input class="l-generator-input" v-model="id" type="text">
+  </div>
+
+  <div class="button-holder">
+    <button class="pwa-button pwa-button--text pwa-button--right" @click="onClickAdd(); $ga.event('Manifest', 'Add Member', 'Related Application', { 'page': `/manifest/add-member` })">
+      {{ $t("related_applications.add") }}
+    </button>
+  </div>
+
+  <div class="l-generator-field l-generator-field--padded"></div>
+
+  <p class="l-generator-error" v-if="error">
+    {{error}}
+  </p>
+
+  <div class="pure-g l-generator-table" v-if="manifest$.related_applications && manifest$.related_applications.length > 0">
+    <div class="pure-u-2-5 l-generator-tableh">{{ $t("related_applications.platform") }}</div>
+    <div class="pure-u-2-5 l-generator-tableh">{{ $t("related_applications.url") }}</div>
+    <div class="pure-u-1-5"></div>
+
+    <div class="pure-u-1" v-for="app in manifest$.related_applications" :key="app.id">
+      <div class="pure-u-2-5 l-generator-tablec">
+        {{app.platform}}
+      </div>
+      <div class="pure-u-2-5 l-generator-tablec">
+        {{app.url}}
+      </div>
+      <div class="pure-u-1-5 l-generator-tablec l-generator-tablec--right">
+        <span class="l-generator-close" @click="onClickRemove(app.id)">
+          <i aria-hidden="true">✕</i>
+        </span>
+      </div>
     </div>
-    <div class="l-generator-field">
-        <label class="l-generator-label">{{ $t("related_applications.url") }}
-            <a class="l-generator-link" href="https://www.w3.org/TR/appmanifest/#url-member" target="_blank">[?]</a>
-        </label>
-        <input class="l-generator-input" v-model="url" type="text">
+  </div>
 
-    </div>
-    <div class="l-generator-field">
-        <label class="l-generator-label">{{ $t("related_applications.id") }}
-            <a class="l-generator-link" href="https://www.w3.org/TR/appmanifest/#id-member" target="_blank">[?]</a>
-        </label>
-        <input class="l-generator-input" v-model="id" type="text">
-
-    </div>
-
-    <div class="button-holder">
-        <button class="pwa-button pwa-button--text pwa-button--right" @click="onClickAdd(); $ga.event('Manifest', 'Add Member', 'Related Application', { 'page': `/manifest/add-member` })">
-            {{ $t("related_applications.add") }}
-        </button>
-    </div>
-
-    <div class="l-generator-field l-generator-field--padded"></div>
-
-    <p class="l-generator-error" v-if="error">
-        {{error}}
-    </p>
-
-    <div class="pure-g l-generator-table" v-if="manifest$.related_applications && manifest$.related_applications.length > 0">
-        <div class="pure-u-2-5 l-generator-tableh">{{ $t("related_applications.platform") }}</div>
-        <div class="pure-u-2-5 l-generator-tableh">{{ $t("related_applications.url") }}</div>
-        <div class="pure-u-1-5"></div>
-
-        <div class="pure-u-1" v-for="app in manifest$.related_applications" :key="app.id">
-            <div class="pure-u-2-5 l-generator-tablec">
-                {{app.platform}}
-            </div>
-            <div class="pure-u-2-5 l-generator-tablec">
-                {{app.url}}
-            </div>
-            <div class="pure-u-1-5 l-generator-tablec l-generator-tablec--right">
-                <span class="l-generator-close" @click="onClickRemove(app.id)">
-                    <i aria-hidden="true">✕</i>
-                </span>
-            </div>
-        </div>
-    </div>
-
-    <div class="l-generator-field">
-        <label class="l-generator-label">
-            <input type="checkbox" class="l-generator-togglecheck" @change="onChangePreferCheckbox($event)">
-            {{ $t("related_applications.prefer") }}
-        </label>
-    </div>
+  <div class="l-generator-field">
+    <label class="l-generator-label">
+      <input type="checkbox" class="l-generator-togglecheck" @change="onChangePreferCheckbox($event)"> {{ $t("related_applications.prefer") }}
+    </label>
+  </div>
 </section>
 </template>
 
@@ -74,49 +72,45 @@ const GeneratorActions = namespace(generator.name, Action);
 
 @Component()
 export default class extends Vue {
-    public manifest$: generator.Manifest | null = null;
-    public platform: string | null = null;
-    public url: string | null = null;
-    public id: string | null = null;
-    public prefer_related_applications = false;
-    public error: string | null = null;
+  public manifest$: generator.Manifest | null = null;
+  public platform: string | null = null;
+  public url: string | null = null;
+  public id: string | null = null;
+  public prefer_related_applications = false;
+  public error: string | null = null;
 
-    @GeneratorState manifest: generator.Manifest;
+  @GeneratorState manifest: generator.Manifest;
+  @GeneratorActions addRelatedApplication;
+  @GeneratorActions removeRelatedApplication;
+  @GeneratorActions changePreferRelatedApplication;
 
-    @GeneratorActions addRelatedApplication;
-    @GeneratorActions removeRelatedApplication;
-    @GeneratorActions changePreferRelatedApplication;
+  public created(): void {
+    this.manifest$ = { ...this.manifest };
+    this.prefer_related_applications = this.manifest$.prefer_related_applications;
+  }
 
-    public created(): void {
-        this.manifest$ = { ...this.manifest };
-        this.prefer_related_applications = this.manifest$.prefer_related_applications;
+  public onClickAdd(): void {
+    try {
+      const relatedApplication = { platform: this.platform, url: this.url, id: this.id };
+      this.addRelatedApplication(relatedApplication);
+
+      this.platform = null;
+      this.url = null;
+      this.id = null;
+
+      this.manifest$ = { ...this.manifest };
+    } catch (e) {
+      this.error = e;
     }
+  }
 
-    public onClickAdd(): void {
-        try {
-            this.addRelatedApplication({
-                platform: this.platform,
-                url: this.url,
-                id: this.id,
-            } as generator.RelatedApplication);
+  public onClickRemove(id: string): void {
+      this.removeRelatedApplication(id);
+  }
 
-            this.platform = null;
-            this.url = null;
-            this.id = null;
-
-            this.manifest$ = { ...this.manifest };
-        } catch (e) {
-            this.error = e;
-        }
-    }
-
-    public onClickRemove(id: string): void {
-        this.removeRelatedApplication(id);
-    }
-
-    public onChangePreferCheckbox($event) {
-        this.changePreferRelatedApplication($event.currentTarget.value);
-    }
+  public onChangePreferCheckbox($event): void {
+      this.changePreferRelatedApplication($event.currentTarget.value);
+  }
 }
 </script>
 
