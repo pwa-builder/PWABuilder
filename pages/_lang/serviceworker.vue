0c7b6bd479a7e5475187530e818f7f10fc9b4307
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-01-03 17:25:03 +0100

    Refactor code structure page serviceworker

--- pages/_lang/serviceworker.vue
+++ pages/_lang/serviceworker.vue
@@ -1,87 +1,84 @@
 <template>
 <section>
-    <GeneratorMenu/>
-    <div class="l-generator-step">
-        <div class="l-generator-semipadded pure-g">
-            <div class="pure-u-1 pure-u-md-1-2 generator-section service-workers">
-                <div class="l-generator-subtitle">{{ $t('serviceworker.title') }}</div>
-                <form @submit.prevent="download" @keydown.enter.prevent="download">
-                    <div class="l-generator-field l-generator-field--padded checkbox">
-                        <label class="l-generator-label">
-                            <input type="radio" value="1" v-model="serviceworker$"> {{ $t('serviceworker.titles.offline_page') }}
-                        </label>
+  <GeneratorMenu/>
+  <div class="l-generator-step">
+    <div class="l-generator-semipadded pure-g">
+      <div class="pure-u-1 pure-u-md-1-2 generator-section service-workers">
+        <div class="l-generator-subtitle">{{ $t('serviceworker.title') }}</div>
+        <form @submit.prevent="download" @keydown.enter.prevent="download">
+          <div class="l-generator-field l-generator-field--padded checkbox">
+            <label class="l-generator-label">
+              <input type="radio" value="1" v-model="serviceworker$"> {{ $t('serviceworker.titles.offline_page') }}
+            </label>
 
-                        <span class="l-generator-description">{{ $t('serviceworker.descriptions.offline_page') }}</span>
-                    </div>
+            <span class="l-generator-description">{{ $t('serviceworker.descriptions.offline_page') }}</span>
+          </div>
 
-                    <div class="l-generator-field l-generator-field--padded checkbox">
-                        <label class="l-generator-label">
-                            <input type="radio" value="2" v-model="serviceworker$"> {{ $t('serviceworker.titles.offline_copy') }}
-                        </label>
+          <div class="l-generator-field l-generator-field--padded checkbox">
+            <label class="l-generator-label">
+              <input type="radio" value="2" v-model="serviceworker$"> {{ $t('serviceworker.titles.offline_copy') }}
+            </label>
 
-                        <span class="l-generator-description">{{ $t('serviceworker.descriptions.offline_copy') }}</span>
-                    </div>
+            <span class="l-generator-description">{{ $t('serviceworker.descriptions.offline_copy') }}</span>
+          </div>
 
-                    <div class="l-generator-field l-generator-field--padded checkbox">
-                        <label class="l-generator-label">
-                            <input type="radio" value="3" v-model="serviceworker$"> {{ $t('serviceworker.titles.offline_copy_backup') }}
-                        </label>
+          <div class="l-generator-field l-generator-field--padded checkbox">
+            <label class="l-generator-label">
+              <input type="radio" value="3" v-model="serviceworker$"> {{ $t('serviceworker.titles.offline_copy_backup') }}
+            </label>
 
-                        <span class="l-generator-description">{{ $t('serviceworker.descriptions.offline_copy_backup') }}</span>
-                    </div>
+            <span class="l-generator-description">{{ $t('serviceworker.descriptions.offline_copy_backup') }}</span>
+          </div>
 
-                    <div class="l-generator-field l-generator-field--padded checkbox">
-                        <label class="pwa-generator-label">
-                            <input type="radio" value="4" v-model="serviceworker$"> {{ $t('serviceworker.titles.cache_first') }}
-                        </label>
+          <div class="l-generator-field l-generator-field--padded checkbox">
+            <label class="pwa-generator-label">
+              <input type="radio" value="4" v-model="serviceworker$"> {{ $t('serviceworker.titles.cache_first') }}
+            </label>
 
-                        <span class="l-generator-description">{{ $t('serviceworker.descriptions.cache_first') }}</span>
-                    </div>
+            <span class="l-generator-description">{{ $t('serviceworker.descriptions.cache_first') }}</span>
+          </div>
 
-                    <div class="l-generator-field l-generator-field--padded checkbox">
-                        <label class="l-generator-label">
-                            <input type="radio" value="5" disabled="" v-model="serviceworker$"> {{ $t('serviceworker.titles.advanced') }}
-                        </label>
+          <div class="l-generator-field l-generator-field--padded checkbox">
+            <label class="l-generator-label">
+              <input type="radio" value="5" disabled="" v-model="serviceworker$"> {{ $t('serviceworker.titles.advanced') }}
+            </label>
 
-                        <span class="l-generator-description">{{ $t('serviceworker.descriptions.advanced') }}</span>
-                    </div>
+            <span class="l-generator-description">{{ $t('serviceworker.descriptions.advanced') }}</span>
+          </div>
 
-                    <div class="l-generator-wrapper pure-u-2-5">
-                        <button @click="$ga.event('Download', 'Web', 'Download Archive', { 'page': `/download/web` })"
-                            class="pwa-button pwa-button--simple isEnabled">
-                            <span v-if="!isBuilding">{{ $t('serviceworker.download') }}</span>
-                            <span v-if="isBuilding">{{ $t('serviceworker.building') }}
-                                <Loading :active="true" class="u-display-inline_block u-margin-left-sm"
-                                />
-                            </span>
-                        </button>
-                    </div>
-                    <div class="pure-u-3-5">
-                        <p class="l-generator-error">{{ $t(error) }}</p>
-                    </div>
-                </form>
-                <p>{{ $t('serviceworker.download_link') }}
-                    <a class="l-generator-link" href="https://github.com/pwa-builder/serviceworkers"
-                        target="_blank">GitHub</a>.</p>
-            </div>
-            <div class="serviceworker-preview pure-u-1 pure-u-md-1-2 generator-section">
-                <CodeViewer :size="viewerSize" :code="webPreview" :title="$t('serviceworker.code_preview_web')">
-                    <nuxt-link :to="$i18n.path('publish')" class="pwa-button pwa-button--simple pwa-button--brand pwa-button--header" @click="$ga.event('item', 'click', 'generator-nextStep-trigger')">
-                        {{ $t("serviceworker.next_step") }}
-                    </nuxt-link>
-                </CodeViewer>
-                <CodeViewer :size="viewerSize" :code="serviceworkerPreview" :title="$t('serviceworker.code_preview_serviceworker')"></CodeViewer>
-            </div>
-        </div>
-    </div>
-    <div class="l-generator-buttons l-generator-buttons--centered">
-        <nuxt-link :to="$i18n.path('publish')" class="pwa-button" @click="$ga.event('item', 'click', 'generator-nextStep-trigger')">
+          <div class="l-generator-wrapper pure-u-2-5">
+            <button @click="$ga.event('Download', 'Web', 'Download Archive', { 'page': `/download/web` })" class="pwa-button pwa-button--simple isEnabled">
+              <span v-if="!isBuilding">{{ $t('serviceworker.download') }}</span>
+              <span v-if="isBuilding">{{ $t('serviceworker.building') }}
+                <Loading :active="true" class="u-display-inline_block u-margin-left-sm" />
+              </span>
+            </button>
+          </div>
+          <div class="pure-u-3-5">
+            <p class="l-generator-error">{{ $t(error) }}</p>
+          </div>
+        </form>
+        <p>{{ $t('serviceworker.download_link') }}
+          <a class="l-generator-link" href="https://github.com/pwa-builder/serviceworkers" target="_blank">GitHub</a>.</p>
+      </div>
+      <div class="serviceworker-preview pure-u-1 pure-u-md-1-2 generator-section">
+        <CodeViewer :size="viewerSize" :code="webPreview" :title="$t('serviceworker.code_preview_web')">
+          <nuxt-link :to="$i18n.path('publish')" class="pwa-button pwa-button--simple pwa-button--brand pwa-button--header" @click="$ga.event('item', 'click', 'generator-nextStep-trigger')">
             {{ $t("serviceworker.next_step") }}
-        </nuxt-link>
+          </nuxt-link>
+        </CodeViewer>
+        <CodeViewer :size="viewerSize" :code="serviceworkerPreview" :title="$t('serviceworker.code_preview_serviceworker')"></CodeViewer>
+      </div>
     </div>
+  </div>
+  <div class="l-generator-buttons l-generator-buttons--centered">
+    <nuxt-link :to="$i18n.path('publish')" class="pwa-button" @click="$ga.event('item', 'click', 'generator-nextStep-trigger')">
+      {{ $t("serviceworker.next_step") }}
+    </nuxt-link>
+  </div>
 
-    <StartOver />
-    <TwoWays/>
+  <StartOver />
+  <TwoWays/>
 </section>
 </template>
 
