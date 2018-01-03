ae1b044a41f061c4fcfef4f50a72d06dc6ba48f4
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-01-03 17:23:29 +0100

    Refactor code structure page index

--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -1,61 +1,51 @@
 <template>
-  <section>
-    <GeneratorMenu :first-link-path="true"/>
-    <div class="l-generator-step">
-      <div class="pure-g l-generator-padded">
-        <div class="pure-u-1 pure-u-md-3-5 pure-u-lg-2-5">
-          <header class="l-generator-header l-generator-header--minimal">
-            <h2 class="l-generator-title">{{ $t('generator.title') }}</h2>
-            <h4 class="l-generator-subtitle">
-              {{ $t('generator.subtitle') }}
-            </h4>
-          </header>
+<section>
+  <GeneratorMenu :first-link-path="true" />
+  <div class="l-generator-step">
+    <div class="pure-g l-generator-padded">
+      <div class="pure-u-1 pure-u-md-3-5 pure-u-lg-2-5">
+        <header class="l-generator-header l-generator-header--minimal">
+          <h2 class="l-generator-title">{{ $t('generator.title') }}</h2>
+          <h4 class="l-generator-subtitle">
+            {{ $t('generator.subtitle') }}
+          </h4>
+        </header>
 
-          <div class="l-generator-form">
-            <form @submit.prevent="checkUrlAndGenerate" @keydown.enter.prevent="checkUrlAndGenerate">
-              <div class="l-generator-field">
-                <label class="l-generator-label" for="siteUrl">{{ $t('generator.url') }}</label>
-                <input 
-                  class="l-generator-input"
-                  :placeholder="$t('generator.placeholder_url')"
-                  name="siteUrl" id="siteUrl"
-                  type="text"
-                  ref="url"
-                  v-model="url$"
-                  autofocus>
+        <div class="l-generator-form">
+          <form @submit.prevent="checkUrlAndGenerate" @keydown.enter.prevent="checkUrlAndGenerate">
+            <div class="l-generator-field">
+              <label class="l-generator-label" for="siteUrl">{{ $t('generator.url') }}</label>
+              <input class="l-generator-input" :placeholder="$t('generator.placeholder_url')" name="siteUrl" id="siteUrl" type="text" ref="url"
+                v-model="url$" autofocus>
+            </div>
+
+            <div class="pure-g l-breath">
+              <div class="l-generator-wrapper pure-u-3-5">
+                <button type="submit" class="get-started pwa-button isEnabled next-step" @click="$ga.event('Build', 'Step 2', 'Scan for Manifest', { 'page': `/build/manifest-scan` })">
+                  {{ $t('generator.start') }}
+                  <Loading :active="inProgress" class="u-display-inline_block u-margin-left-sm"/>
+                </button>
               </div>
 
-              <div class="pure-g l-breath">
-                <div class="l-generator-wrapper pure-u-3-5">
-                    <button
-                      type="submit"
-                      class="get-started pwa-button isEnabled next-step"
-                      @click="$ga.event('Build', 'Step 2', 'Scan for Manifest', { 'page': `/build/manifest-scan` })">
-                      {{ $t('generator.start') }} <Loading :active="inProgress" class="u-display-inline_block u-margin-left-sm" />
-                    </button>
-                </div>
-
-                <div class="pure-u-2-5">
-                   <p class="l-generator-error">{{ $t(error) }}</p>
-                </div>
-
-                <div class="l-generator-wrapper pure-u-1">
-                  <button 
-                    @click="skipCheckUrl(); $ga.event('Skip', 'Manifest', 'Skip to Service Worker', { 'page': `/skip/service-worker` })"
-                    class="pwa-button pwa-button--simple">
-                    {{ $t('generator.skip') }}
-                  </button>
-                </div>
+              <div class="pure-u-2-5">
+                <p class="l-generator-error">{{ $t(error) }}</p>
               </div>
-              <p class="l-narrow">{{ $t('generator.skip_description') }}</p>
-              
-            </form>
-          </div>
+
+              <div class="l-generator-wrapper pure-u-1">
+                <button @click="skipCheckUrl(); $ga.event('Skip', 'Manifest', 'Skip to Service Worker', { 'page': `/skip/service-worker` })"
+                  class="pwa-button pwa-button--simple">
+                  {{ $t('generator.skip') }}
+                </button>
+              </div>
+            </div>
+            <p class="l-narrow">{{ $t('generator.skip_description') }}</p>
+          </form>
         </div>
       </div>
     </div>
-    <TwoWays/>
-  </section>
+  </div>
+  <TwoWays/>
+</section>
 </template>
 
 <script lang='ts'>
@@ -85,7 +75,6 @@ export default class extends Vue {
   public error: string | null = null;
 
   @GeneratorState url: string;
-
   @GeneratorAction updateLink;
   @GeneratorAction getManifestInformation;
 
