b42b9e184a4ffb1d84ebdf7d9d2e1683ff83ee12
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-01-03 16:57:50 +0100

    Refactor code structure CustomMember

--- components/CustomMembers.vue
+++ components/CustomMembers.vue
@@ -1,40 +1,42 @@
 <template>
 <section>
-    <div class="l-generator-field">
-        <label class="l-generator-label">{{ $t("custom_members.name") }}
-        </label>
-        <input class="l-generator-input" v-model="name" type="text">
-    </div>
-    <div class="l-generator-field">
-        <label class="l-generator-label">{{ $t("custom_members.value") }}
-        </label>
-        <input class="l-generator-input" v-model="value" type="text">
-    </div>
+  <div class="l-generator-field">
+    <label class="l-generator-label">
+      {{ $t("custom_members.name") }}
+    </label>
+    <input class="l-generator-input" v-model="name" type="text">
+  </div>
+  <div class="l-generator-field">
+    <label class="l-generator-label">
+      {{ $t("custom_members.value") }}
+    </label>
+    <input class="l-generator-input" v-model="value" type="text">
+  </div>
 
-    <div class="button-holder">
-        <button class="pwa-button pwa-button--text pwa-button--right" @click="onClickAdd(); $ga.event('Manifest', 'Add Member', 'Add a Member', { 'page': '/manifest/add-member' })">
-            {{ $t("custom_members.add") }}
-        </button>
+  <div class="button-holder">
+    <button class="pwa-button pwa-button--text pwa-button--right" @click="onClickAdd(); $ga.event('Manifest', 'Add Member', 'Add a Member', { 'page': '/manifest/add-member' })">
+      {{ $t("custom_members.add") }}
+    </button>
+  </div>
+
+  <div class="l-generator-field l-generator-field--padded"></div>
+
+  <p class="l-generator-error" v-if="error">
+    {{error}}
+  </p>
+
+  <div class="pure-g l-generator-table" v-if="members$ && members$.length > 0">
+    <div class="pure-u-1" v-for="member in members$" :key="member.name">
+      <div class="pure-u-2-5 l-generator-tablec">
+        {{member.name}}
+      </div>
+      <div class="pure-u-3-5 l-generator-tablec l-generator-tablec--right">
+        <span class="l-generator-close" @click="onClickRemove(member.name)">
+          <i aria-hidden="true">✕</i>
+        </span>
+      </div>
     </div>
-
-    <div class="l-generator-field l-generator-field--padded"></div>
-
-    <p class="l-generator-error" v-if="error">
-        {{error}}
-    </p>
-
-    <div class="pure-g l-generator-table" v-if="members$ && members$.length > 0">
-        <div class="pure-u-1" v-for="member in members$" :key="member.name">
-            <div class="pure-u-2-5 l-generator-tablec">
-                {{member.name}}
-            </div>
-            <div class="pure-u-3-5 l-generator-tablec l-generator-tablec--right">
-                <span class="l-generator-close" @click="onClickRemove(member.name)">
-                    <i aria-hidden="true">✕</i>
-                </span>
-            </div>
-        </div>
-    </div>
+  </div>
 </section>
 </template>
 
@@ -50,38 +52,40 @@ const GeneratorActions = namespace(generator.name, Action);
 
 @Component()
 export default class extends Vue {
-    public members$: generator.CustomMember[] | null = null;
-    public name: string | null = null;
-    public value: string | null = null;
-    public error: string | null = null;
+  public members$: generator.CustomMember[] | null = null;
+  public name: string | null = null;
+  public value: string | null = null;
+  public error: string | null = null;
 
-    @GeneratorState members: generator.CustomMember[];
+  @GeneratorState members: generator.CustomMember[];
 
-    @GeneratorActions addCustomMember;
-    @GeneratorActions removeCustomMember;
+  @GeneratorActions addCustomMember;
+  @GeneratorActions removeCustomMember;
 
-    public created(): void {
-        this.members$ = [...this.members ];
+  public created(): void {
+    this.members$ = [...this.members ];
+  }
+
+  public onClickAdd(): void {
+    if (!this.name || !this.value) {
+      return;
     }
 
-    public onClickAdd(): void {
-        try {
-            this.addCustomMember({
-                name: this.name,
-                value: this.value
-            } as generator.CustomMember);
+    try {
+      const customMember: generator.CustomMember = { name: this.name, value: this.value };
+      this.addCustomMember(customMember);
 
-            this.name = null;
-            this.value = null;
+      this.name = null;
+      this.value = null;
 
-            this.members$ = [...this.members ];
-        } catch (e) {
-            this.error = e;
-        }
+      this.members$ = [...this.members ];
+    } catch (e) {
+      this.error = e;
     }
+  }
 
-    public onClickRemove(name: string): void {
-        this.removeCustomMember(name);
-    }
+  public onClickRemove(name: string): void {
+    this.removeCustomMember(name);
+  }
 }
 </script>
