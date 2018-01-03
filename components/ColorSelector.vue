ef5fb21a555d7ff7eb0b1ccaab7c39ba4fd1bc10
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-01-03 16:55:08 +0100

    Refactor code structure ColorSelector

--- components/ColorSelector.vue
+++ components/ColorSelector.vue
@@ -1,18 +1,20 @@
 <template>
 <div class="l-generator-field">
-    <label class="l-generator-label">{{ $t("generate.background_color") }}
-        <a class="l-generator-link" href="https://www.w3.org/TR/appmanifest/#background_color-member" target="_blank">[?]</a>
+  <label class="l-generator-label">
+    {{ $t("generate.background_color") }}
+    <a class="l-generator-link" href="https://www.w3.org/TR/appmanifest/#background_color-member" target="_blank">[?]</a>
+  </label>
+  <div class="l-generator-options">
+    <label class="l-generator-label" v-for="colorOption in colorOptions" :key="colorOption">
+      <input type="radio" :value="colorOption" :name='id' @change="onChangeColor(colorOption)" :checked="isColorChecked(colorOption)"> {{ $t("generate." + colorOption) }}
     </label>
-    <div class="l-generator-options">
-        <label class="l-generator-label" v-for="colorOption in colorOptions" :key="colorOption">
-            <input type="radio" :value="colorOption" :name='id' @change="onChangeColor(colorOption)" :checked="isColorChecked(colorOption)"> {{ $t("generate." + colorOption) }}
-        </label>
 
-        <div v-if="canChooseColor">
-            <input class="l-generator-input l-generator-input--tiny" type="color" name="background_color" v-model="color">
-            <input v-if="checkInputColor" class="l-generator-input l-generator-input--small" type="text" placeholder="#000000" name="background_color" v-model="color">
-        </div>
+    <div v-if="canChooseColor">
+      <input class="l-generator-input l-generator-input--tiny" type="color" name="background_color" v-model="color">
+      <input v-if="checkInputColor" class="l-generator-input l-generator-input--small" type="text" placeholder="#000000" name="background_color"
+        v-model="color">
     </div>
+  </div>
 </div>
 </template>
 
@@ -28,67 +30,69 @@ const GeneratorActions = namespace(generator.name, Action);
 
 @Component()
 export default class extends Vue {
-    public manifest$: generator.Manifest | null = null;
+  public manifest$: generator.Manifest | null = null;
 
-    public colorOptions = generator.helpers.COLOR_OPTIONS;
-    public id: string | null = null;
-    public color: string | null = null;
-    public canChooseColor = false;
+  public colorOptions = generator.helpers.COLOR_OPTIONS;
+  public id: string | null = null;
+  public color: string | null = null;
+  public canChooseColor = false;
 
-    @GeneratorState manifest: generator.Manifest;
-    @GeneratorActions updateColor;
+  @GeneratorState manifest: generator.Manifest;
+  @GeneratorActions updateColor;
 
-    public get checkInputColor(): boolean {
-       if (typeof document === 'undefined') {
-                return false;
-        }
-
-        let i = document.createElement('input');
-        i.setAttribute('type', 'color');
-        return i.type !== 'text';
+  public get checkInputColor(): boolean {
+    if (typeof document === 'undefined') {
+      return false;
     }
 
-    public created(): void {
-        this.id = 'color-selector' + Date.now();
-        this.manifest$ = { ...this.manifest };
+    let i = document.createElement('input');
+    i.setAttribute('type', 'color');
+    return i.type !== 'text';
+  }
 
-        this.updateCanChooseColor();
+  public created(): void {
+    this.id = 'color-selector' + Date.now();
+    this.manifest$ = { ...this.manifest };
 
-        // Check manifest from server color
-        if (this.canChooseColor) {
-            this.color = this.color || this.manifest$.theme_color || this.manifest$.background_color;
-        }
+    this.updateCanChooseColor();
+
+    // Check manifest from server color
+    if (this.canChooseColor) {
+      this.color = this.color || this.manifest$.theme_color || this.manifest$.background_color;
+    }
+  }
+
+  private updateCanChooseColor(): void {
+    if (!this.manifest$) {
+        return;
     }
 
-    private updateCanChooseColor(): void {
-        if (!this.manifest$) {
-            return;
-        }
+    this.canChooseColor = this.manifest$.background_color !== this.colorOptions.none && this.manifest$.background_color !== this.colorOptions.transparent;
+  }
 
-        this.canChooseColor = this.manifest$.background_color !== this.colorOptions.none && this.manifest$.background_color !== this.colorOptions.transparent;
+  public isColorChecked(colorOption: string): boolean {
+    if (!this.manifest$) {
+      return false;
     }
 
-    public isColorChecked(colorOption: string): boolean {
-        if (!this.manifest$) {
-            return false;
-        }
-
-        if (colorOption === this.colorOptions.pick && this.manifest$.background_color !== this.colorOptions.none && this.manifest$.background_color !== this.colorOptions.transparent) {
-            return true;
-        }
-
-        return this.manifest$.background_color === colorOption;
+    if (colorOption === this.colorOptions.pick && this.manifest$.background_color !== this.colorOptions.none && this.manifest$.background_color !== this.colorOptions.transparent) {
+      return true;
     }
 
-    public onChangeColor(colorOption: string): void {
-        this.updateColor({
-            colorOption,
-            color: this.color,
-        } as generator.ColorOptions);
+    return this.manifest$.background_color === colorOption;
+  }
 
-        this.manifest$ = { ...this.manifest };
-
-        this.updateCanChooseColor();
+  public onChangeColor(colorOption: string): void {
+    if (!this.color) {
+      return;
     }
+
+    const colorOptions: generator.ColorOptions = { colorOption, color: this.color };
+    this.updateColor(colorOptions);
+
+    this.manifest$ = { ...this.manifest };
+
+    this.updateCanChooseColor();
+  }
 }
 </script>
