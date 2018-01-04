0267e3c5356eafb701c6842afce898805bed7186
Quique Fdez Guerra <efernandez@plainconcepts.com>
2018-01-04 12:39:02 +0100

    Use https for production api

--- environments/production.js
+++ environments/production.js
@@ -1,5 +1,5 @@
 module.exports = {
     baseUrl: process.env.BASE_URL,
-    apiUrl: 'http://manifold-api-prod.azurewebsites.net',
+    apiUrl: 'https://manifold-api-prod.azurewebsites.net',
     analytics: 'UA-98003629-1'
 }
