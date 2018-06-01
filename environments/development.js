c445d62f156023671666d0656b1bdb06fdbfc00c
Lucas Rojas <lucas.rojas@southworks.com>
2018-06-01 13:45:33 -0300

    Add comming soon message to Advance pre cache service worker

--- environments/development.js
+++ environments/development.js
@@ -1,6 +1,5 @@
 module.exports = {
-    //apiUrl: 'http://manifold-api-pre.azurewebsites.net',
-    apiUrl: 'http://localhost:3001',
+    apiUrl: 'http://manifold-api-pre.azurewebsites.net',
     analytics: 'UA-111887945-1',
     apiUrl2: 'https://appimagegenerator-pre.azurewebsites.net'
 }
