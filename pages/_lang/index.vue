cbc5f848cf124002d6af89fe2b3f71448c8636da
Justin Willis <juwillis@microsoft.com>
2018-09-04 12:17:11 -0700

    update card component and use it on index page

--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <section>
+  <div id="wrapperDiv">
+    <section class="indexSection">
+      <div class="line"></div>
       <h2>How it Works</h2>
     </section>
 
-    <section>
+    <section class="indexSection">
+      <div class="line"></div>
       <h2>Other Useful Tools</h2>
+
+      <div id="cardBar">
+        <Card title="Windows APIs" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet.."/>
+        <Card title="Image Generator" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet.."/>
+        <Card title="Knowledge Base" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet.."/>
+      </div>
     </section>
   </div>
 </template>
@@ -14,7 +22,13 @@
 import Vue from 'vue';
 import Component from 'nuxt-class-component';
 
-@Component({})
+import Card from '~/components/Card.vue';
+
+@Component({
+  components: {
+    Card
+  }
+})
 export default class extends Vue {
 }
 </script>
@@ -25,6 +39,26 @@ export default class extends Vue {
   h2 {
     color: #838383;
     height: 30px;
+  }
+
+  .indexSection {
+    margin-left: 92px;
+    margin-right: 92px;
+  }
+
+  .line {
+    background: #D8D8D8;
+    height: 2px;
+    width: 100%;
+  }
+
+  #wrapperDiv {
+    margin-top: 27px;
+  }
+
+  #cardBar {
+    display: flex;
+    justify-content: space-around;
   }
 
 </style>
