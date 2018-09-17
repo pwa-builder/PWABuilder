43eb21138286f8e67ce412e584526fe7078c1118
Justin Willis <jgw9617@gmail.com>
2018-09-17 13:38:38 -0700

    add new "what makes a good PWA" section to front page

--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -3,38 +3,74 @@
 <section>
   <div class="l-generator-step">
     <div class="pure-g padding">
-        <section id="getStartedBlock">
-          <div id="quickTextBlock">
-            <h2 id="quickBlockText">Quickly and easily turn your website into an app</h2>
+      <section id="getStartedBlock">
+        <div id="quickTextBlock">
+          <h2 id="quickBlockText">Quickly and easily turn your website into an app</h2>
 
-            <p id="quickBlockPlaceholder">Lorem ipsum so dolor sit amet etc and a quick summary about what a PWA is. Also a link to more information.</p>
+          <p id="quickBlockPlaceholder">Lorem ipsum so dolor sit amet etc and a quick summary about what a PWA is. Also a link to more information.</p>
+        </div>
+
+        <div id="bottomBlock">
+          <div id="leftBlock">
+            <h2 id="getStartedHere">Get Started!</h2>
+
+            <p id='placeholderText'>Enter the URL of the website you want to convert to a Progressive Web App below</p>
+
+            <form @submit.prevent="checkUrlAndGenerate" @keydown.enter.prevent="checkUrlAndGenerate">
+              <input id="getStartedInput" :aria-label="$t('generator.url')" :placeholder="$t('generator.placeholder_url')" name="siteUrl" type="text" ref="url"
+                v-model="url$" autofocus />
+
+              <button @click=" $awa( { 'referrerUri': 'https://preview.pwabuilder.com/build/manifest-scan' })" id="getStartedButton">
+                {{ $t('generator.start') }}
+              </button>
+            </form>
           </div>
 
-          <div id="bottomBlock">
-            <div id="leftBlock">
-              <h2 id="getStartedHere">Get Started!</h2>
-
-              <p id='placeholderText'>Enter the URL of the website you want to convert to a Progressive Web App below</p>
-
-              <form @submit.prevent="checkUrlAndGenerate" @keydown.enter.prevent="checkUrlAndGenerate">
-                <input id="getStartedInput" :aria-label="$t('generator.url')" :placeholder="$t('generator.placeholder_url')" name="siteUrl" type="text" ref="url"
-                    v-model="url$" autofocus />
-
-                <button @click=" $awa( { 'referrerUri': 'https://preview.pwabuilder.com/build/manifest-scan' })" id="getStartedButton">
-                  {{ $t('generator.start') }}
-                </button>
-              </form>
-            </div>
-
-            <div id="alreadyPWA">
-              <h3>Or if you're already familiar with PWAs...</h3>
-              <a href="">Take me straight to the APIs, extensions, and other good stuff!</a>
-            </div>
+          <div id="alreadyPWA">
+            <h3>Or if you're already familiar with PWAs...</h3>
+            <a href="">Take me straight to the APIs, extensions, and other good stuff!</a>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
-    <GeneratorMenu :first-link-path="true" />
+  </div>
+
+  <GeneratorMenu :first-link-path="true" />
+
+  <div class="l-generator-step">
+    <div id="whatMakesBlock">
+      <div id="goodPWAHeaderBlock">
+        <h2 id="quickBlockText">What makes a good PWA?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque auctor arcu non felis ultricies, ut viverra eros accumsan. Aenean eget lorem luctus, aliquet diam eu, imperdiet nisi. Donec quis justo nisl. Cras quis arcu sapien.</p>
+      </div>
+
+      <section id="goodBetterBar">
+        <div>
+          <h3>Good</h3>
+          <ul>
+            <li>Uses a fully completed manifest</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Better</h3>
+          <ul>
+            <li>Uses a fully completed manifest</li>
+            <li>Uses Service Workers to enable offline use cases</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Best</h3>
+          <ul>
+            <li>Uses a fully completed manifest</li>
+            <li>Uses Service Workers to enable offline use cases</li>
+            <li>Integrates with native features in the operating system</li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  </div>
 </section>
 </template>
 
@@ -127,6 +163,13 @@ Vue.prototype.$awa = function(config) {
     width: 100%;
   }
 
+  #whatMakesBlock {
+    padding-bottom: 48px;
+    padding-left: 68px;
+    padding-right: 68px;
+    padding-top: 65px;
+  }
+
   #quickBlockText {
     font-size: 32px;
     font-weight: 600;
@@ -160,7 +203,8 @@ Vue.prototype.$awa = function(config) {
 
   #quickTextBlock,
   #leftBlock,
-  #alreadyPWA {
+  #alreadyPWA,
+  #goodPWAHeaderBlock {
     width: 472px;
   }
   
@@ -181,6 +225,16 @@ Vue.prototype.$awa = function(config) {
     margin-left: 8px;
     padding: 10px;
     text-align: center;
+  }
+
+  #goodBetterBar {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  #goodBetterBar h3 {
+    font-size: 32px;
+    font-weight: 400;
   }
 </style>
 
