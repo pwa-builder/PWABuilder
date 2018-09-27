1121764cf93bf552167f4a88185b4ce719711efd
Jeff Burtoft <boyofgreen@gmail.com>
2018-09-27 10:38:25 -0500

    Update preview.js

--- environments/preview.js
+++ environments/preview.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiUrl: 'https://manifold-api-pre-staging.azurewebsites.net',
+    apiUrl: 'https://manifold-api-pre.azurewebsites.net',
     analytics: 'UA-98003629-1',
-    apiUrl2: 'https://appimagegenerator-pre-staging.azurewebsites.net'
+    apiUrl2: 'https://appimagegenerator-pre.azurewebsites.net'
 }
