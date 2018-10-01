48a240b0fafebf46b886a595aab1745a45e1c2c8
Justin Willis <jgw9617@gmail.com>
2018-10-01 14:15:34 -0700

    each item can be toggled between good and bad

--- components/GoodPWA.vue
+++ components/GoodPWA.vue
@@ -9,18 +9,18 @@
     <div>
       <h3>{{ $t('home.quality_low_title') }}</h3>
       <ul>
-        <li>{{ $t('home.quality_low_list_1') }}</li>
-        <li>{{ $t('home.quality_low_list_2') }}</li>
-        <li>{{ $t('home.quality_low_list_3') }}</li>
+        <li v-bind:class="{ good: isHttps }">{{ $t('home.quality_low_list_1') }}</li>
+        <li v-bind:class="{ good: hasManifest }">{{ $t('home.quality_low_list_2') }}</li>
+        <li v-bind:class="{ good: hasWorker }">{{ $t('home.quality_low_list_3') }}</li>
       </ul>
     </div>
 
     <div>
       <h3>{{ $t('home.quality_mid_title') }}</h3>
       <ul>
-        <li>{{ $t('home.quality_mid_list_1') }}</li>
-        <li>{{ $t('home.quality_mid_list_2') }}</li>
-        <li>{{ $t('home.quality_mid_list_3') }}</li>
+        <li v-bind:class="{ good: hasBetterWorker }">{{ $t('home.quality_mid_list_1') }}</li>
+        <li v-bind:class="{ good: hasBetterManifest }">{{ $t('home.quality_mid_list_2') }}</li>
+        <li v-bind:class="{ good: isResponsive }">{{ $t('home.quality_mid_list_3') }}</li>
       </ul>
     </div>
 
@@ -28,9 +28,9 @@
       <h3>{{ $t('home.quality_high_title') }}</h3>
       <ul>
         <li>{{ $t('home.quality_high_list_1') }}</li>
-        <li>Uses a fully completed manifest</li>
-        <li>Uses Service Workers to enable offline use cases</li>
-        <li>Integrates with native features in the operating system</li>
+        <li v-bind:class="{ good: hasBestManifest }">Uses a fully completed manifest</li>
+        <li v-bind:class="{ good: hasBestWorker }">Uses Service Workers to enable offline use cases</li>
+        <li v-bind:class="{ good: hasNativeFeatures }">Integrates with native features in the operating system</li>
       </ul>
     </div>
   </div>
@@ -44,8 +44,15 @@ import { Prop } from 'vue-property-decorator';
 
 @Component({})
 export default class extends Vue {
-  @Prop({}) title: string;
-  @Prop({}) description: string;
+  @Prop({}) isHttps: boolean;
+  @Prop({}) hasManifest: boolean;
+  @Prop({}) hasWorker: boolean;
+  @Prop({}) hasBetterManifest: boolean;
+  @Prop({}) hasBetterWOrker: boolean;
+  @Prop({}) hasBestManifest: boolean;
+  @Prop({}) hasBestWorker: boolean;
+  @Prop({}) hasNativeFeatures: boolean;
+  @Prop({}) isResponsive: boolean;
 }
 </script>
 
@@ -72,5 +79,9 @@ export default class extends Vue {
 
 #goodPWAHeaderBlock {
   width: 472px;
+}
+
+.good {
+  color: $color-brand;
 }
 </style>
