e81186c26eeee148c07132ccb0e639840dc8edd0
Justin Willis <jgw9617@gmail.com>
2018-11-01 10:46:56 -0700

    fix for weird service worker issue i ran into in Edge

--- static/pwabuilder-sw-register.js
+++ static/pwabuilder-sw-register.js
@@ -2,7 +2,7 @@
 
 //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 if (navigator.serviceWorker.controller) {
-    console.log('[PWA Builder] active service worker found, no need to register')
+    console.log('[PWA Builder] active service worker found, no need to register');
   } else {
     //Register the ServiceWorker
     navigator.serviceWorker.register('pwabuilder-sw.js', {
