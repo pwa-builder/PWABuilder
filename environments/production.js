e3582367979c3360361eeb2d4bba7fff5819c837
Justin Willis <jgw9617@gmail.com>
2019-01-29 12:03:26 -0800

    make sure were pointing to the right endpoint everywhere

--- environments/production.js
+++ environments/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiUrl: 'https://manifold-api-pre.azurewebsites.net',
+    apiUrl: 'http://pwabuilder-api-pre.azurewebsites.net',
     analytics: 'UA-98003629-1',
     apiUrl2: 'https://appimagegenerator-prod.azurewebsites.net'
 }
