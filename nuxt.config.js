6963224c60491547f4cef59720cdfeb46fab29f2
Justin Willis <jgw9617@gmail.com>
2019-04-11 14:11:47 -0700

    chore(): update for add to homescreen

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,8 +17,8 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'All the tools you need to build and deploy your Progressive Web Apps.' },
       { name: 'msapplication-TileImage', content: '/Images/assets/icons/ms-icon-144x144-487a503e5cb29bbe0df7296db4093b7e.png' },
-      { name: 'msapplication-TileColor', content: '#9337D8' },
-      { name: 'theme-color', content: '#9337D8' },
+      { name: 'msapplication-TileColor', content: '#3c3c3c' },
+      { name: 'theme-color', content: '#000000' },
       { name: 'ms.appid', content: 'PWABuilder' }
 
     ],
