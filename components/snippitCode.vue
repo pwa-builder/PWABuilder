1b9658e695185bcec36a9929407058763aa291fc
Mariano Stinson <marianst.dt@gmail.com>
2019-06-14 17:16:57 -0300

    Refactor merge PR issues

--- components/snippitCode.vue
+++ components/snippitCode.vue
@@ -16,8 +16,7 @@
       </button>
     </div>
 
-    <monaco-editor class="codeDiv" v-model="actualCode" language="typescript"></monaco-editor>
-
+    <div id="codeDiv" ref="monacoDiv"></div>
   </div>
 </template>
 
@@ -25,65 +24,53 @@
 import Vue from "vue";
 import { Prop } from "vue-property-decorator";
 import Component from "nuxt-class-component";
-import MonacoEditor from "vue-monaco";
-
 import Clipboard from "clipboard";
-
+import * as monaco from "monaco-editor";
 @Component({})
 export default class extends Vue {
   @Prop({ type: String, default: null }) snippitURL;
   @Prop({ type: String, default: null }) codeType;
-
   public actualCode: string = "";
-  public editor: MonacoEditor.editor.IStandaloneCodeEditor;
-
+  public editor: monaco.editor.IStandaloneCodeEditor;
   async mounted() {
     const response = await fetch(this.snippitURL);
     console.log(response);
     const data = await response.text();
-
     console.log("snippit data", data);
-
     this.actualCode = data;
-
-    // monaco.editor.defineTheme(`snippitTheme`, {
-    //   base: "vs",
-    //   inherit: true,
-    //   rules: [],
-    //   colors: {
-    //     "editor.background": "#F0F0F0"
-    //   }
-    // });
-
-  //   this.editor = monaco.editor.create(this.$refs.monacoDiv as HTMLElement, {
-  //     value: this.actualCode,
-  //     // Turn line numbers on so that line numbers in errors make sense
-  //     language: this.codeType,
-  //     lineNumbers: "on",
-  //     fixedOverflowWidgets: true,
-  //     wordWrap: "wordWrapColumn",
-  //     wordWrapColumn: 50,
-  //     scrollBeyondLastLine: false,
-  //     // Set this to false to not auto word wrap minified files
-  //     wordWrapMinified: true,
-
-  //     // try "same", "indent" or "none"
-  //     wrappingIndent: "indent",
-  //     fontSize: 16,
-  //     theme: "snippitTheme",
-  //     minimap: {
-  //       enabled: false
-  //     }
-  //   });
+    monaco.editor.defineTheme(`snippitTheme`, {
+      base: "vs",
+      inherit: true,
+      rules: [],
+      colors: {
+        "editor.background": "#F0F0F0"
+      }
+    });
+    this.editor = monaco.editor.create(this.$refs.monacoDiv as HTMLElement, {
+      value: this.actualCode,
+      // Turn line numbers on so that line numbers in errors make sense
+      language: this.codeType,
+      lineNumbers: "on",
+      fixedOverflowWidgets: true,
+      wordWrap: "wordWrapColumn",
+      wordWrapColumn: 50,
+      scrollBeyondLastLine: false,
+      // Set this to false to not auto word wrap minified files
+      wordWrapMinified: true,
+      // try "same", "indent" or "none"
+      wrappingIndent: "indent",
+      fontSize: 16,
+      theme: "snippitTheme",
+      minimap: {
+        enabled: false
+      }
+    });
   }
-
   goToGithub() {
     window.open(this.snippitURL, "_blank");
   }
-
   async copy() {
     const code = this.editor.getValue();
-
     if ((navigator as any).clipboard) {
       try {
         await (navigator as any).clipboard.writeText(code);
@@ -92,14 +79,12 @@ export default class extends Vue {
       }
     } else {
       let clipboard = new Clipboard(code);
-
       clipboard.on("success", e => {
         console.info("Action:", e.action);
         console.info("Text:", e.text);
         console.info("Trigger:", e.trigger);
         e.clearSelection();
       });
-
       clipboard.on("error", e => {
         console.error("Action:", e.action);
         console.error("Trigger:", e.trigger);
@@ -110,11 +95,9 @@ export default class extends Vue {
 </script>
 
 <style lang="scss" scoped>
-
 #snippitCodeHeader {
   display: flex;
 }
-
 h4 {
   font-weight: bold;
   margin-top: 1em;
@@ -124,12 +107,10 @@ h4 {
   color: #E2E2E2;
   justify-content: center;
 }
-
 p {
   font-size: 14px;
   margin-bottom: 24px;
 }
-
 #monacoHeader {
   background: #E2E2E2;
   display: flex;
@@ -137,7 +118,6 @@ p {
   padding-top: 11px;
   padding-bottom: 11px;
   padding-right: 24px;
-
   #githubButton {
     height: 32px;
     width: 32px;
@@ -145,7 +125,6 @@ p {
     border: none;
     background: white;
   }
-
   #copyButton {
     width: 32px;
     border: none;
@@ -155,10 +134,8 @@ p {
     margin-right: 10px;
   }
 }
-
 #codeDiv {
   height: 11em;
   margin-bottom: 24px;
 }
-
 </style>
