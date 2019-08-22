c8ee6a323c9b9320aad840fd96df5407f99c5a6a
Justin Willis <jgw9617@gmail.com>
2019-08-22 12:22:40 -0700

    chore(): force prod image server

--- environments/development.js
+++ environments/development.js
@@ -2,5 +2,5 @@ module.exports = {
     apiUrl: 'https://pwabuilder-api-prod.azurewebsites.net',
     // apiUrl: 'http://localhost:3333',
     analytics: 'UA-111887945-1',
-    apiUrl2: 'https://appimagegenerator-pre.azurewebsites.net'
+    apiUrl2: 'https://appimagegenerator-prod.azurewebsites.net'
 }
