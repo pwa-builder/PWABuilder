1969cf078e9e768dc73acb2b55917bf7611deeb2
Justin Willis <jgw9617@gmail.com>
2020-01-15 14:51:23 -0800

    chore(): update to latest pwainstall version

--- nuxt.config.js
+++ nuxt.config.js
@@ -146,7 +146,7 @@ module.exports = {
         src: 'https://unpkg.com/@ionic/core@latest/dist/ionic.js'
       },
       {
-        src: 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwainstall@1.2.8',
+        src: 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwainstall@1.2.9',
         type: 'module'
       }
     ]
