5e1c4002e370987092c8e744543499614ca38794
Amrutha Srinivasan <amsrin@microsoft.com>
2020-06-08 08:12:34 -0700

    Changedurl

--- environments/development.js
+++ environments/development.js
@@ -1,6 +1,6 @@
 module.exports = {
-  //apiUrl: 'https://pwabuilder-api-prod.azurewebsites.net',
-  apiUrl: 'http://localhost:3333',
+  apiUrl: 'https://pwabuilder-api-prod.azurewebsites.net',
+  //apiUrl: 'http://localhost:3333',
   screenshotsUrl: 'https://pwa-screenshots.azurewebsites.net',
   analytics: 'UA-111887945-1',
   apiUrl2: 'https://appimagegenerator-pre.azurewebsites.net',
