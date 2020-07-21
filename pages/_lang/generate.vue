54d41a6412766714009a50e7d811285574e00895
lee-leonardo <lee.leonardo.king@gmail.com>
2020-07-20 18:41:03 -0700

    tabs and selection. does left and right on osx, but should work on ms

--- pages/_lang/generate.vue
+++ pages/_lang/generate.vue
@@ -18,24 +18,45 @@
           <div id="dataButtonsBlock">
             <div id="dataButtons" role="tablist">
               <button
+                id="infoTabButton"
                 v-bind:class="{ active: showBasicSection }"
                 @click="showBasicsSection()"
                 role="tab"
+                aria-controls="infoTab"
+                aria-label="Info"
+                :aria-selected="showBasicSection ? 'true' : 'false'"
+                tabindex="0"
               >Info</button>
               <button
+                id="imagesTabButton"
                 v-bind:class="{ active: showImagesSection }"
                 @click="showImageSection()"
                 role="tab"
+                aria-label="Images"
+                aria-controls="imagesTab"
+                :aria-selected="showImagesSection ? 'true' : 'false'"
+                tabindex="0"
               >Images</button>
               <button
+                id="settingsTabButton"
                 v-bind:class="{ active: showSettingsSection }"
                 @click="showSettingSection()"
                 role="tab"
+                aria-label="Settings"
+                aria-controls="settingsTab"
+                :aria-selected="showSettingsSection ? 'true' : 'false'"
+                tabindex="0"
               >Settings</button>
             </div>
           </div>
 
-          <section class="animatedSection" v-if="showBasicSection">
+          <section
+            id="infoTab"
+            class="animatedSection"
+            role="tabpanel"
+            aria-labelledby="infoTabButton"
+            v-if="showBasicSection"
+          >
             <div class="l-generator-field">
               <label class="l-generator-label">
                 <h4
@@ -121,7 +142,13 @@
             </div>
           </section>
 
-          <section class="animatedSection" v-if="showImagesSection">
+          <section
+            id="imagesTab"
+            class="animatedSection"
+            role="tabpanel"
+            aria-labelledby="imagesTabButton"
+            v-if="showImagesSection"
+          >
             <div class="l-generator-field logo-upload">
               <div id="uploadNewSection">
                 <label class="l-generator-label">
@@ -301,7 +328,13 @@
             </div>
           </section>
 
-          <section class="animatedSection" v-if="showSettingsSection">
+          <section
+            id="settingsTag"
+            class="animatedSection"
+            role="tabpanel"
+            aria-labelledby="settingsTabButton"
+            v-if="showSettingsSection"
+          >
             <div class="l-generator-field">
               <label class="l-generator-label">
                 <h4
