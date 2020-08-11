477d519bc4457099fc13c0f0f60fe4497d76dbd2
lee-leonardo <lee.leonardo.king@gmail.com>
2020-08-11 15:24:13 -0700

    invoking min-width media queries to isolate defined features to help facilitate the zoom accessibility issues

--- pages/_lang/reportCard.vue
+++ pages/_lang/reportCard.vue
@@ -787,7 +787,9 @@ declare var awa: any;
 main {
   @include grid;
 
-  margin-bottom: 2em;
+  @media (min-width: 801px) {
+    margin-bottom: 2em;
+  }
 }
 
 h2 {
@@ -800,7 +802,10 @@ h2 {
   font-size: 24px;
   line-height: 54px;
   letter-spacing: -0.02em;
-  height: 36px;
+
+  @media (min-width: 801px) {
+    height: 36px;
+  }
 }
 
 #topHalfHeader {
@@ -818,8 +823,6 @@ h2 {
 
   #topHalfHome {
     grid-row: 1;
-
-    margin-top: 68px;
 
     form {
       display: flex;
@@ -881,16 +884,8 @@ h2 {
     }
   }
 
-  @media (max-height: 375px) {
-    #topHalfHome {
-      margin-top: 24px;
-    }
-  }
-
-  @media (max-height: 320px) {
-    #topHalfHome {
-      margin-top: 4px;
-    }
+  @media (min-width: 801px) {
+    margin-top: 68px;
   }
 
   @media (max-width: 425px) {
@@ -939,7 +934,6 @@ h2 {
       color: black;
       padding: 1.4em;
       border-radius: 6px;
-      width: 24em;
 
       h2 {
         margin: 0;
@@ -973,11 +967,9 @@ h2 {
           border: none;
         }
       }
-    }
 
-    @media (max-width: 640px) {
-      #starterSection {
-        display: none;
+      @media (min-width: 641px) {
+        width: 24em;
       }
     }
   }
@@ -996,6 +988,34 @@ h2 {
   }
 }
 
+@media (max-width: 800px) {
+  #main {
+    display: flex;
+    grid-template-columns: none;
+    margin-bottom: 0;
+  }
+
+  #inputSection {
+    display: flex;
+    grid-template-rows: none;
+    flex-direction: column;
+  }
+
+  #bottomHalfHome {
+    margin-top: 16px;
+  }
+}
+
+@media (max-width: 640px) {
+  #topHalfHome {
+    margin-top: 4px;
+  }
+
+  #starterSection {
+    width: 100%;
+  }
+}
+
 @media (max-width: 425px) {
   #inputSection {
     display: initial;
@@ -1007,6 +1027,7 @@ h2 {
 
   #inputSection #bottomHalfHome {
     width: initial;
+    padding: 0 25px;
   }
 
   footer {
@@ -1296,10 +1317,6 @@ h2 {
 
   .backgroundReport {
     @include backgroundRightPoint(80%, 25vh);
-  }
-
-  #starterSection {
-    display: none;
   }
 
   footer {
