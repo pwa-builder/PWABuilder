72848413d52a5ea207b51cc47dd0140c0ed0bd79
Justin Willis <jgw9617@gmail.com>
2020-10-07 11:15:18 -0700

    chore(): join with a .

--- utils/windows-utils.ts
+++ utils/windows-utils.ts
@@ -26,7 +26,7 @@ export function generateWindowsPackageId(host: string): string {
     .filter(p => p.length > 0)
     .map(p => p.replace(DISALLOWED_WINDOWS_PACKAGE_CHARS_REGEX, "-"))
   parts.push("edge");
-  return parts.join("-");
+  return parts.join(".");
 }
 
 export function validateWindowsOptions(options) {
