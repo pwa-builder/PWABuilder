82dc46debf6d39854480ed630799b6f0c0f0bd06
Judah Gabriel Himango <judahgabriel@gmail.com>
2020-10-23 14:20:39 -0700

    Updated Windows package options

--- utils/windows-utils.ts
+++ utils/windows-utils.ts
@@ -1,6 +1,11 @@
 import { WindowsPublisherOptions, WindowsPackageOptions } from '~/store/modules/publish';
 import { validateUrl } from '~/utils/android-utils';
 
+type WindowsPackageValidationError = {
+    field: keyof WindowsPackageOptions | keyof WindowsPublisherOptions | null;
+    error: string;
+};
+
 export function validatePackageID(id: string) {
   if (id && id.length >= 2) {
     try {
@@ -29,8 +34,8 @@ export function generateWindowsPackageId(host: string): string {
   return parts.join(".");
 }
 
-export function validateWindowsOptions(options) {
-  const validationErrors: { field: keyof WindowsPackageOptions | null, error: string }[] = [];
+export function validateWindowsOptions(options: WindowsPackageOptions): WindowsPackageValidationError[] {
+  const validationErrors: WindowsPackageValidationError[] = [];
   if (!options) {
     validationErrors.push({ field: null, error: "No options specified " });
     return validationErrors;
@@ -41,43 +46,35 @@ export function validateWindowsOptions(options) {
   }
 
   if (!options.name || options.name.trim().length === 0) {
-    validationErrors.push({ field: "name", error: "Must have an app name" });
+    validationErrors.push({ field: "name", error: "App name required" });
   }
-  else if (options.name.trim().length > 256) {
+  else if (options.name.trim().length >= 256) {
     validationErrors.push({ field: "name", error: "App name must be less than 256 characters" });
   }
 
   if (!options.version || options.version.trim().length === 0 && !options.classicPackage.version || options.classicPackage.version.trim().length === 0) {
     validationErrors.push({ field: "version", error: "Must have an app version and a Classic Package version" });
-  }
-  else {
-    validateWindowsPackageVersion(options.version, options.classicPackage.version, validationErrors);
-  }
-
-  // Validating publisher options when we have publisher data
-  if (!options.publisher) {
-    validationErrors.push({ field: "publisher", error: "Publisher information must be supplied." });
   } else {
-    // commonName and displayName are required, all other publisher fields are optional
-    const requiredPublisherFields: Array<keyof WindowsPublisherOptions> = [
-      "displayName",
-      "commonName"
-    ];
+    validationErrors.push(...validateWindowsPackageVersion(options.version.trim(), options.classicPackage.version.trim()));
+  }
 
-    requiredPublisherFields
-      .filter(prop => !options.publisher![prop])
-      .forEach(() => validationErrors.push({ field: "publisher", error: `Publisher info must be specified` }));
+  // Validating publisher options
+  if (!options.publisher) {
+    validationErrors.push({ field: "publisher", error: "Publisher information required." });
+  } else {
+    if (!options.publisher.commonName) {
+      validationErrors.push({ field: "commonName", error: "Publisher ID required." });
+    }
 
-    // Ensure country code is 2 chars
-    if (options.publisher.country && options.publisher.country.length !== 2) {
-      validationErrors.push({ field: "publisher", error: "Publisher country code must be 2 letters" });
+    if (!options.publisher.displayName) {
+      validationErrors.push({ field: "displayName", error: "Publisher display name required." });
     }
   }
 
   if (!options.url) {
     validationErrors.push({ field: "url", error: "Start URL must be specified. If your start URL is the same as Host, you can use '/' as the start URL." });
   } else {
-    const startUrlError = validateUrl(options.url, options.host);
+    const startUrlError = validateUrl(options.url);
     if (startUrlError) {
       validationErrors.push({ field: "url", error: "URL is invalid" });
     }
@@ -86,33 +83,37 @@ export function validateWindowsOptions(options) {
   return validationErrors;
 }
 
-function validateWindowsPackageVersion(version: string, classicVersion: string, validationErrors: { field: keyof WindowsPackageOptions | null, error: string }[]) {
-  console.log(version.trim(), version.trim().length);
+function validateWindowsPackageVersion(version: string, classicVersion: string): WindowsPackageValidationError[] {
+  const versionErrors: WindowsPackageValidationError[] = [];
+  
+  // Common validation run on both version and classic version.
+  const versionInfos = [
+    { name: "version" as keyof WindowsPackageOptions, label: "Version", value: version },
+    { name: "classicVersion" as keyof WindowsPackageOptions, label: "Classic version", value: classicVersion }
+  ];
+  for (let versionInfo of versionInfos) {
+    // Version must be 3 segments ("1.0.0") - the 4th segment is reserved for Store use.
+    const segments = versionInfo.value.split(".");
+    if (segments.length !== 3) {
+      versionErrors.push({ field: versionInfo.name, error: `${versionInfo.label} must have 3 segments: 1.0.0.` });
+    }
 
-  /**
-   * Version must be in this form: "1.0.0"; a 3 segment version. 
-   * 4 segment versions (e.g. 1.0.0.0) are invalid due to Store restrictions; 
-   * the Store reserves the 4th segment for internal use.
-   * 
-   */
-  if (version.trim().split(".").length > 3 || version.trim().split(".").length < 3) {
-    validationErrors.push({ field: "version", error: "Version must be in this form: 1.0.0; a 3 segment version." });
+    // All the segments must be numbers.
+    if (segments.some(s => !s.match(/^(0|[1-9][0-9]*)$/))) {
+      versionErrors.push({ field: versionInfo.name, error: `${versionInfo.label} must only contain integers separated by periods.`})
+    }
+
+    // Version must be 1.0.0 or greater; Store doesn't support versions starting with zero.
+    const segmentValues = segments.map(s => parseInt(s));
+    if (segmentValues.length > 0 && segmentValues[0] <= 0) {
+      versionErrors.push({ field: versionInfo.name, error: `${versionInfo.label} must start with an integer >= 1.` })
+    }
   }
 
-  /**
-   * Version must be 1.0.0 or greater; Store doesn't support versions starting with zero.
-   */
-  const firstChar = version.trim().substr(0, 1);
-
-  if (firstChar && parseInt(firstChar) < 1) {
-    validationErrors.push({ field: "version", error: "Version must be 1.0.0 or greater; Store doesn't support versions starting with zero." });
+  // Make sure the version is > classic version
+  if (version <= classicVersion) {
+    versionErrors.push({ field: "version", error: "App version must be greater than classic package version" });
   }
 
-  /**
-   * classic package version should always be lower than app version
-   */
-
-  if (version.trim() < classicVersion.trim()) {
-    validationErrors.push({ field: "version", error: "Classic package version must always be lower than the App version" });
-  }
+  return versionErrors;
 }
