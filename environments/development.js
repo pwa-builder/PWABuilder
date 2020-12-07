20ffba7f89c954b91a26fbe14205a44b3cde185c
lee-leonardo <lee.leonardo.king@gmail.com>
2020-12-07 11:49:32 -0800

    env change

--- environments/development.js
+++ environments/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   apiUrl: 'https://pwabuilder-api-pre.azurewebsites.net',
-  //apiUrl: 'http://localhost:3333',
+  // apiUrl: 'http://localhost:3333',
   screenshotsUrl: 'https://pwa-screenshots.azurewebsites.net',
   analytics: 'UA-111887945-1',
   imageGeneratorUrl: 'https://appimagegenerator-pre.azurewebsites.net',
@@ -8,5 +8,5 @@ module.exports = {
   testAPIUrl: 'https://pwabuilder-tests-dev.azurewebsites.net/api',
   manifestFinderUrl: "https://pwabuilder-manifest-finder.azurewebsites.net/api/findmanifest",
   windowsPackageGeneratorUrl: 'https://pwabuilder-win-chromium-platform.centralus.cloudapp.azure.com/msix/generatezip',
-  
+  macosPackageGeneratorUrl: 'https://pwabuilder-macos.azurewebsites.net/',  
 };
