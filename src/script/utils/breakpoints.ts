4a1168fbfda90826c67668e1da03b0226a8b481a
lee-leonardo <lee.leonardo.king@gmail.com>
2021-01-08 17:07:53 -0800

    change back to current impl

--- src/script/utils/breakpoints.ts
+++ src/script/utils/breakpoints.ts
@@ -1,68 +1,51 @@
-import { css, unsafeCSS, CSSResult } from 'lit-element'
+import { unsafeCSS, CSSResult } from 'lit-element';
 
-const constructionToken = Symbol()
+const constructionToken = Symbol();
 
-export function smallBreakPoint(template: TemplateStringsArray, ...values: (CSSResult | number)[]) {
-  return css`
+export function smallBreakPoint(styles: CSSResult) {
+  return unsafeCSS(`
     @media screen and (max-width: 479px) {
-      ${reducer(template, ...values)}
+      ${styles}
     }
-  `
+  `);
 }
 
-export function mediumBreakPoint(template: TemplateStringsArray, ...values: (CSSResult|number)[]) {
-  return css`
+export function mediumBreakPoint(styles: CSSResult) {
+  return unsafeCSS(`
     @media screen and (min-width: 480px) and (max-width: 639px) {
-      ${reducer(template, ...values)}
+      ${styles}
     }
-  `
+  `);
 }
 
-export function largeBreakPoint(template: TemplateStringsArray, ...values: (CSSResult|number)[]) {
-  return css`
+export function largeBreakPoint(styles: CSSResult) {
+  return unsafeCSS(`
     @media screen and (min-width: 640px) and (max-width: 1023px) {
-      ${reducer(template, ...values)}
+      ${styles}
     }
-  `
+  `);
 }
 
-export function xLargeBreakPoint(template: TemplateStringsArray, ...values: (CSSResult|number)[]) {
-  return css`
+export function xLargeBreakPoint(styles: CSSResult) {
+  return unsafeCSS(`
     @media screen and (min-width: 1024px) and (max-width: 1365px) {
-      ${reducer(template, ...values)}
+      ${styles}
     }
-  `
+  `);
 }
 
-export function xxLargeBreakPoint(template: TemplateStringsArray, ...values: (CSSResult|number)[]) {
-  return css`
+export function xxLargeBreakPoint(styles: CSSResult) {
+  return unsafeCSS(`
     @media screen and (min-width: 1366px) and (max-width: 1919px) {
-      ${reducer(template, ...values)}
+      ${styles}
     }
-  `
+  `);
 }
 
-export function xxxLargeBreakPoint(template: TemplateStringsArray, ...values: (CSSResult|number)[]) {
-  return css`
+export function xxxLargeBreakPoint(styles: CSSResult) {
+  return unsafeCSS(`
     @media screen and (min-width: 1920px) {
-      ${reducer(template, ...values)}
+      ${styles}
     }
-  `
-}
-
-function reducer(template: TemplateStringsArray, ...values: (CSSResult | number)[]) {
-  return unsafeCSS(values.reduce(
-    (acc, v, idx) => acc + templateText(v) + template[idx + 1],
-    template[0]
-  ));
-}
-
-function templateText(value: CSSResult | number) {
-  if (value instanceof CSSResult) {
-    return value.cssText
-  } else if (typeof value === "number") {
-    return String(value)
-  } else {
-    throw Error("Unsafe usage of template literal")
-  }
+  `);
 }
