b06d39b3267467bac66497ec16bc80ae396da80a
lee-leonardo <lee.leonardo.king@gmail.com>
2021-01-08 17:04:52 -0800

    tried making them into tag functions, but the way the css class is implemented makes it hard to super class. Therefore I just went with the unsafeCSS route, but force the end user to use the css tag function to add the styles

--- src/script/utils/breakpoints.ts
+++ src/script/utils/breakpoints.ts
@@ -1,49 +1,51 @@
 import { unsafeCSS, CSSResult } from 'lit-element';
 
+const constructionToken = Symbol();
+
 export function smallBreakPoint(styles: CSSResult) {
-  return unsafeCSS(`
+  return unsafeCSS`
     @media screen and (max-width: 479px) {
       ${styles}
     }
-  `);
+  `;
 }
 
 export function mediumBreakPoint(styles: CSSResult) {
-  return unsafeCSS(`
+  return unsafeCSS`
     @media screen and (min-width: 480px) and (max-width: 639px) {
       ${styles}
     }
-  `);
+  `;
 }
 
 export function largeBreakPoint(styles: CSSResult) {
-  return unsafeCSS(`
+  return unsafeCSS`
     @media screen and (min-width: 640px) and (max-width: 1023px) {
       ${styles}
     }
-  `);
+  `;
 }
 
 export function xLargeBreakPoint(styles: CSSResult) {
-  return unsafeCSS(`
+  return unsafeCSS`
     @media screen and (min-width: 1024px) and (max-width: 1365px) {
       ${styles}
     }
-  `);
+  `;
 }
 
 export function xxLargeBreakPoint(styles: CSSResult) {
-  return unsafeCSS(`
+  return unsafeCSS`
     @media screen and (min-width: 1366px) and (max-width: 1919px) {
       ${styles}
     }
-  `);
+  `;
 }
 
 export function xxxLargeBreakPoint(styles: CSSResult) {
-  return unsafeCSS(`
+  return unsafeCSS`
     @media screen and (min-width: 1920px) {
       ${styles}
     }
-  `);
+  `;
 }
