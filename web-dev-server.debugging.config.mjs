4fdd8f45d17a2b293db130ebd0252d44374fa79f
Judah Gabriel Himango <judahgabriel@gmail.com>
2021-01-13 13:49:11 -0800

    Removed unused package

--- web-dev-server.debugging.config.mjs
+++ web-dev-server.debugging.config.mjs
@@ -1,3 +1,4 @@
+// This is the configuration file for `npm debug`
 // This is the same as web-server.config.mjs, except that it doesn't launch a browser, see the `open: false`
 // The browser will instead be launched by VSCode F5 debugging in a separate user profile.
 
