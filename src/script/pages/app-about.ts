907973b158de1cf4bf02f7b8200812b4a7159223
jgw96 <jgw9617@gmail.com>
2021-01-21 12:56:32 -0800

    feat(): new app-alert component

--- src/script/pages/app-about.ts
+++ src/script/pages/app-about.ts
@@ -2,6 +2,11 @@ import { LitElement, css, html, customElement } from 'lit-element';
 import { testManifest } from '../services/tests/manifest';
 import { testServiceWorker } from '../services/tests/service-worker';
 
+// double import needed to get full impl
+// and type
+import '../components/app-alert';
+import { AppAlert } from '../components/app-alert';
+
 @customElement('app-about')
 export class AppAbout extends LitElement {
   static get styles() {
@@ -25,10 +30,27 @@ export class AppAbout extends LitElement {
     }
   }
 
+  testAlert(ev: any) {
+    console.log(ev);
+    const alert: AppAlert | null | undefined = this.shadowRoot?.querySelector("app-alert");
+
+    if (alert) {
+      alert.openAlert(ev.clientX, ev.clientY);
+    }
+  }
+
   render() {
     return html`
       <div>
         <h2>About Page</h2>
+
+        <fast-button @click="${(ev: any) => this.testAlert(ev)}">test alert</fast-button>
+
+        <app-alert title="Test Alert">
+          <p>Info description. Lorem ipsum dolor sit amet, consectetur elit adipiscing, sed do eiusm tem. Ipsum dolor sit.</p>
+
+          <fast-anchor slot="actions">Test</fast-anchor>
+        </app-alert>
       </div>
     `;
   }
