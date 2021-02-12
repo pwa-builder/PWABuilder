6359a8c88e31d904f2ceac21f33c313b66a08dda
lee-leonardo <lee.leonardo.king@gmail.com>
2021-02-12 15:43:33 -0800

    adding visiblity so that elements don't change alignment

--- src/script/utils/css/hidden.ts
+++ src/script/utils/css/hidden.ts
@@ -1,9 +1,17 @@
 import { css } from 'lit-element';
 
+export const hidden = css`
+  .hidden {
+    display: none;
+    visibility: none;
+  }
+`;
+
 export const hidden_sm = css`
   @media screen and (max-width: 479px) {
     .hidden-sm {
       display: none;
+      visibility: none;
     }
   }
 `;
@@ -12,6 +20,7 @@ export const hidden_med = css`
   @media screen and (min-width: 480px) and (max-width: 639px) {
     .hidden-med {
       display: none;
+      visibility: none;
     }
   }
 `;
@@ -20,6 +29,7 @@ export const hidden_lrg = css`
   @media screen and (min-width: 640px) and (max-width: 1023px) {
     .hidden-lrg {
       display: none;
+      visibility: none;
     }
   }
 `;
@@ -28,6 +38,7 @@ export const hidden_xlrg = css`
   @media screen and (min-width: 1024px) and (max-width: 1365px) {
     .hidden-xlrg {
       display: none;
+      visibility: none;
     }
   }
 `;
@@ -36,6 +47,7 @@ export const hidden_xxlrg = css`
   @media screen and (min-width: 1366px) and (max-width: 1919px) {
     .hidden-xxlrg {
       display: none;
+      visibility: none;
     }
   }
 `;
@@ -44,6 +56,7 @@ export const hidden_xxxlrg = css`
   @media screen and (min-width: 1920px) {
     .hidden-xxxlrg {
       display: none;
+      visibility: none;
     }
   }
 `;
