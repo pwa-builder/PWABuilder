5c8ecce32501d40c1adcd2ca90746c7e04d33125
lee-leonardo <lee.leonardo.king@gmail.com>
2021-02-17 11:34:27 -0800

    add generator url

--- src/script/utils/environment.ts
+++ src/script/utils/environment.ts
@@ -5,5 +5,6 @@ export const env = {
   testAPIUrl: "https://pwabuilder-tests-dev.azurewebsites.net/api",
   windowsPackageGeneratorUrl: 'https://pwabuilder-win-chromium-platform.centralus.cloudapp.azure.com/msix/generatezip',
   androidPackageGeneratorUrl: 'https://pwabuilder-cloudapk-pre.azurewebsites.net',
-  safeUrlFetcher: 'https://pwabuilder-safe-url.azurewebsites.net/api/getsafeurl'
+  safeUrlFetcher: 'https://pwabuilder-safe-url.azurewebsites.net/api/getsafeurl',
+  imageGeneratorUrl: "https://appimagegenerator-prod.azurewebsites.net/api/image"
 }
