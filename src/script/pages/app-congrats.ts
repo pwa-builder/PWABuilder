1e2cebc8324d8a66a7c9359562d7a73587100a9c
Justin Willis <jgw9617@gmail.com>
2021-03-24 11:35:33 -0700

    feat(): display platform choices based on generated platforms

--- src/script/pages/app-congrats.ts
+++ src/script/pages/app-congrats.ts
@@ -7,7 +7,11 @@ import {
 } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map';
 
-import { BreakpointValues, xxxLargeBreakPoint, largeBreakPoint } from '../utils/css/breakpoints';
+import {
+  BreakpointValues,
+  xxxLargeBreakPoint,
+  largeBreakPoint,
+} from '../utils/css/breakpoints';
 
 // @ts-ignore
 import style from '../../../styles/layout-defaults.css';
@@ -15,6 +19,7 @@ import style from '../../../styles/layout-defaults.css';
 import '../components/app-header';
 import '../components/app-sidebar';
 import '../components/content-header';
+import { getPlatformsGenerated } from '../services/congrats';
 
 @customElement('app-congrats')
 export class AppCongrats extends LitElement {
@@ -23,6 +28,10 @@ export class AppCongrats extends LitElement {
   );
 
   @internalProperty() isDeskTopView = this.mql.matches;
+
+  @internalProperty() generatedPlatforms = undefined;
+
+  @internalProperty() generating = false;
 
   static get styles() {
     return [
@@ -49,10 +58,36 @@ export class AppCongrats extends LitElement {
           max-width: 526px;
         }
 
+        h3 {
+          font-size: var(--medium-font-size);
+          margin-bottom: 8px;
+        }
+
         #hero-p {
           font-size: var(--font-size);
           line-height: 24px;
           max-width: 406px;
+        }
+
+        ul {
+          list-style: none;
+          margin: 0;
+          padding: 0;
+
+          width: 100%;
+        }
+
+        li {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding-top: 35px;
+          padding-bottom: 35px;
+          border-bottom: var(--list-border);
+        }
+
+        li h4 {
+          font-size: var(--small-medium-font-size);
         }
 
         ${xxxLargeBreakPoint(
@@ -90,6 +125,15 @@ export class AppCongrats extends LitElement {
     super();
   }
 
+  firstUpdated() {
+    this.generatedPlatforms = getPlatformsGenerated();
+    console.log(this.generatedPlatforms);
+  }
+
+  showWindowsOptionsModal() {}
+
+  generatePackage(platform: string) {}
+
   render() {
     return html`
       <div>
@@ -119,6 +163,57 @@ export class AppCongrats extends LitElement {
             </content-header>
 
             <app-sidebar id="tablet-sidebar"></app-sidebar>
+
+            <section id="summary-block">
+              <h3>Nice</h3>
+
+              <p>
+                REcap and intoduction to the other assets we have that can
+                further their PWAs. Nemo enim ipsam voluptatem quia voluptas sit
+                aspernatur aut odit aut fugit, sed quia consequuntur magni
+                dolores eos qui ratione voluptatem sequi nesciunt. ven further!
+              </p>
+            </section>
+
+            <section id="other-stores">
+              <h3>Publish your PWA to other stores?</h3>
+
+              <ul>
+                ${this.generatedPlatforms &&
+                this.generatedPlatforms.windows === false
+                  ? html`
+                      <li>
+                        <div id="title-block">
+                          <h4>Windows</h4>
+                          <p>
+                            Some text about how awesome PWAs are on Windows and
+                            how you should publish to the Microsoft Store
+                          </p>
+                        </div>
+
+                        <div id="platform-actions-block">
+                          <app-button
+                            @click="${() => this.showWindowsOptionsModal()}"
+                            >Publish</app-button
+                          >
+
+                          <loading-button
+                            ?loading=${this.generating}
+                            id="test-package-button"
+                            @click="${() => this.generatePackage('windows')}"
+                            >Test Package</loading-button
+                          >
+                        </div>
+                      </li>
+                    `
+                  : null}
+              </ul>
+
+              ${this.generatedPlatforms &&
+              this.generatedPlatforms.android === false
+                ? html``
+                : null}
+            </section>
           </div>
         </div>
       </div>
--- src/script/services/congrats.ts
+++ src/script/services/congrats.ts
@@ -1,15 +1,17 @@
-import { android_generated } from "./publish/android-publish";
-import { web_generated } from "./publish/web-publish";
-import { windows_generated } from "./publish/windows-publish";
+import { android_generated } from './publish/android-publish';
+import { web_generated } from './publish/web-publish';
+import { windows_generated } from './publish/windows-publish';
 
 export function getPlatformsGenerated() {
-    const android = android_generated;
-    const windows = windows_generated;
-    const web = web_generated;
+  const android = android_generated;
+  const windows = windows_generated;
+  const web = web_generated;
 
-    return {
-        android,
-        windows,
-        web
-    }
+  console.log('android', android);
+
+  return {
+    android,
+    windows,
+    web,
+  };
 }
