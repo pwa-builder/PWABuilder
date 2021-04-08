3b6e3159fefef21184a271214d822133bdc73274
Justin Willis <jgw9617@gmail.com>
2021-04-08 15:25:31 -0700

    feat(): badges have locked style in badges card

--- src/script/components/app-badges.ts
+++ src/script/components/app-badges.ts
@@ -5,6 +5,8 @@ import {
   customElement,
   internalProperty,
 } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
+
 import { getCurrentBadges, getPossibleBadges } from '../services/badges';
 
 @customElement('app-badges')
@@ -15,6 +17,8 @@ export class AppBadges extends LitElement {
   @internalProperty() possible_badges:
     | Array<{ name: string; url: string }>
     | undefined;
+
+    duplicate: Array<string>;
 
   static get styles() {
     return css`
@@ -36,6 +40,10 @@ export class AppBadges extends LitElement {
         font-weight: var(--font-bold);
         text-align: center;
       }
+
+      .locked {
+        opacity: 0.5;
+      }
     `;
   }
 
@@ -46,6 +54,29 @@ export class AppBadges extends LitElement {
   firstUpdated() {
     this.possible_badges = getPossibleBadges();
     this.current_badges = getCurrentBadges();
+
+    const combined: Array<string> = [];
+
+    this.possible_badges.forEach(badge => {
+      combined.push(badge.url);
+    });
+
+    this.current_badges.forEach(badge => {
+      combined.push(badge.url);
+    });
+
+    console.log('here in app-badges');
+
+    this.duplicate = combined.reduce(
+      (acc: Array<string>, currentValue, index, array) => {
+        if (array.indexOf(currentValue) != index && !acc.includes(currentValue))
+          acc.push(currentValue);
+        return acc;
+      },
+      []
+    );
+
+    console.log('duplicate', this.duplicate);
   }
 
   render() {
@@ -53,7 +84,11 @@ export class AppBadges extends LitElement {
       <div id="badges-container">
         ${this.possible_badges?.map(badge => {
           return html`
-            <div class="badge">
+            <div
+              class="badge ${classMap({
+                'locked': this.duplicate.includes(badge.url) ? false : true,
+              })}"
+            >
               <img .src="${badge.url}" .alt="${badge.name} icon" />
 
               <span>${badge.name}</span>
