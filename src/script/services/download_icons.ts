791e438b5bf0798038dc71ed968814d4a40a696d
lee-leonardo <lee.leonardo.king@gmail.com>
2021-04-20 15:33:34 -0700

    dl code for icons, using service

---
+++ src/script/services/download_icons.ts
@@ -0,0 +1,27 @@
+import { download } from '../utils/download';
+import { Icon } from '../utils/interfaces';
+
+const url = 'https://azure-express-zip-creator.azurewebsites.net/api';
+
+export async function generateAndDownloadIconZip(images: Array<Icon>) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: new Headers({
+        'content-type': 'application/json',
+      }),
+      body: JSON.stringify({
+        images,
+      }),
+    });
+
+    const blob = await response.blob();
+    await download({
+      id: 'icon',
+      fileName: 'pwabuilder-icons',
+      blob,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
