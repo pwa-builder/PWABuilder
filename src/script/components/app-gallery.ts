2d62af3615411e0121765886bc87f9f3a9970a93
lee-leonardo <lee.leonardo.king@gmail.com>
2021-04-22 14:14:01 -0700

    basic testing atm, the disabled buttons are working for hte most part... need to change the styles and etc.

--- src/script/components/app-gallery.ts
+++ src/script/components/app-gallery.ts
@@ -5,22 +5,46 @@ import {
   customElement,
   property,
   internalProperty,
+  query,
 } from 'lit-element';
-import { Icon, Lazy } from '../utils/interfaces';
-import { AppButtonElement } from '../utils/interfaces.components';
+import { Lazy } from '../utils/interfaces';
 
+import './app-modal';
 import './app-button';
 
-@customElement('app-button')
+@customElement('app-gallery')
 export class AppGallery extends LitElement {
-  @property({ type: Array }) images: Lazy<Array<Icon>>;
+  @property({ attribute: 'images', type: Array }) images: Lazy<Array<string>>;
   @property({ type: Number }) index = 0;
+  @internalProperty() modalOpened = false;
+
+  @query('.gallery') gallery: Lazy<HTMLElement>;
 
   static get styles() {
     return css`
       :host {
       }
+
+      .gallery {
+        display: flex;
+        flex-direction: row;
+        vertical-align: middle;
+        align-items: center;
+      }
+
+      .image-container {
+        max-height: 216px;
+        max-width: 344px;
+      }
     `;
+  }
+
+  get currentImage() {
+    if (!this.images) {
+      return undefined;
+    }
+
+    return this.images[this.index] ?? undefined;
   }
 
   constructor() {
@@ -31,20 +55,45 @@ export class AppGallery extends LitElement {
     return html`
       <div class="gallery ${this.className}">
         <app-button
+          class="round"
           @click=${this.navigateBack}
-          .disabled=${this.backDisabled()}
+          ?disabled=${this.backDisabled()}
         >
           <ion-icon name="chevron-back-outline"></ion-icon>
         </app-button>
-        <span class="image-container">
-          <img .src=${this.images[this.index].src} />
+        <span class="image-container" @click=${this.openModal}>
+          <!-- <img
+            class="current-image"
+            decoding="async"
+            loading="lazy"
+            .src=${this.currentImage}
+          /> -->
+          <div style="background-color: red; width: 100px; height: 100px">
+            ${this.index}
+          </div>
         </span>
         <app-button
+          class="round"
           @click=${this.navigateForward}
-          .disabled=${this.forwardDisabled()}
+          ?disabled=${this.forwardDisabled()}
         >
           <ion-icon name="chevron-forward-outline"></ion-icon>
         </app-button>
+        <app-modal ?open=${this.modalOpened}>
+          <!-- <img
+            slot="modal-image"
+            class="modal-image"
+            decoding="async"
+            loading="lazy"
+            .src=${this.currentImage}
+          /> -->
+          <div style="background-color: red; width: 100px; height: 100px">
+            ${this.index}
+          </div>
+          <div slot="modal-actions">
+            <app-button @click=${this.closeModal}> Close </app-button>
+          </div>
+        </app-modal>
       </div>
     `;
   }
@@ -62,6 +111,14 @@ export class AppGallery extends LitElement {
   }
 
   forwardDisabled() {
-    return this.index == this.images.length - 1;
+    return this.index == this.images?.length - 1;
+  }
+
+  openModal() {
+    this.modalOpened = true;
+  }
+
+  closeModal() {
+    this.modalOpened = false;
   }
 }
