65d8e7b82590dc54adf5a39ac6bc6b4dc9182af4
Judah Gabriel Himango <judahgabriel@gmail.com>
2021-04-27 18:23:27 -0700

    Added offline check fallback

--- utils/service-worker-checker.ts
+++ utils/service-worker-checker.ts
@@ -38,28 +38,62 @@ export class ServiceWorkerChecker {
      * Checks the URL for offline support.
      */
     async detectOfflineSupport(): Promise<boolean> {
+        // We have 2 offline checks:
+        // - A Google Lighthouse-based check, run via APIv2
+        // - A Puppeteer check check, run via our service worker API.
+        // We'll run both and see if we get success on either.
+        // Additionally, we time box this to 10 seconds because we've witnessed very long timeouts or hangs for some sites.
+
+        return new Promise<boolean>(resolve => {
+            const resolveIfOfflineDetected = (offlineDetected: boolean) => {
+                if (offlineDetected) {
+                    resolve(true);
+                }
+            };
+
+            // Race to success: if any test returns offline = true, use that.
+            // Otherwise, punt if we timeout, or if both tests return false.
+            const puppeteerCheck = this.detectOfflineSupportPuppeteer();
+            const lighthouseCheck = this.detectOfflineSupportLighthouse();
+            new Promise<void>(() => setTimeout(() => resolve(false), 10000));
+
+            puppeteerCheck
+                .then(result => resolveIfOfflineDetected(result));
+            lighthouseCheck
+                .then(result => resolveIfOfflineDetected(result));
+
+            // If both checks finished, resolve as no offline detected.
+            Promise["allSettled"]([puppeteerCheck, lighthouseCheck])
+                .then(() => resolve(false));
+        });
+    }
+
+    private async detectOfflineSupportLighthouse(): Promise<boolean> {
+        const fetchResult = await fetch(`${this.offlineCheckUrl}/offline/?site=${encodeURIComponent(this.url)}`);
+        if (!fetchResult.ok) {
+            console.warn("Unable to detect offline support via Lighthouse.", fetchResult.status, fetchResult.statusText);
+            throw new Error(fetchResult.statusText);
+        }
+
         type OfflineCheckResult = {
             data: {
                 offline: boolean;
             }
         };
-
-        // We've witnessed this call take a very long time. We're going to time-box it to 10s.
-        const tenSecondTimeout = new Promise<void>(resolve => setTimeout(() => resolve(), 10000));
-        const offlineFetch = fetch(`${this.offlineCheckUrl}/offline/?site=${encodeURIComponent(this.url)}`);
-
-        const fetchResultOrTimeout = await Promise.race([tenSecondTimeout, offlineFetch]);
-        if (!fetchResultOrTimeout) {
-            console.warn("Offline check timed out after 10 seconds.");
-            throw new Error("Offline check timed out after 10 seconds.");
-        }
-        if (!fetchResultOrTimeout.ok) {
-            console.warn("Unable to detect offline support.", fetchResultOrTimeout.status, fetchResultOrTimeout.statusText);
-            throw new Error(fetchResultOrTimeout.statusText);
-        }
-
-        const jsonResult: OfflineCheckResult = await fetchResultOrTimeout.json();
-        console.info("Offline support detection succeeded. Offline support =", jsonResult);
+        const jsonResult: OfflineCheckResult = await fetchResult.json();
+        console.info("Offline support detection completed via Lighthouse. Offline support =", jsonResult);
         return jsonResult.data.offline;
+    }
+
+    private async detectOfflineSupportPuppeteer(): Promise<boolean> {
+        const fetchResult = await fetch(`${this.serviceWorkerCheckUrl}/serviceworker/GetOfflineSupport?url=${encodeURIComponent(this.url)}`);
+        if (!fetchResult.ok) {
+            console.warn("Unable to detect offline support via Puppeteer.", fetchResult.status, fetchResult.statusText);
+            throw new Error(fetchResult.statusText);
+        }
+
+        const jsonResult: boolean = await fetchResult.json();
+        console.info("Offline support detection completed via Puppeteer. Offline support =", jsonResult);
+        return jsonResult;
     }
 }
