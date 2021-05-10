c1f5fc500da9fd998b6012e96ef558d809754954
Justin Willis <jgw9617@gmail.com>
2021-05-10 11:49:15 -0700

    fix(): dont try to cache json (#1698)

--- workbox.mjs
+++ workbox.mjs
@@ -3,7 +3,7 @@ import { injectManifest } from 'workbox-build';
 injectManifest({
     globDirectory: 'dist',
     globPatterns: [
-      '**/*.{html,json,js,css,png,webp,jpg}',
+      '**/*.{html,js,css,png,webp,jpg}',
     ],
     swSrc: 'dist/pwabuilder-sw.js',
     swDest: 'dist/pwabuilder-sw.js',
