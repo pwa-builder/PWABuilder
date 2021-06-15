524f327b1a1512ee587e942ced9883c4a6ca1915
lee-leonardo <lee.leonardo.king@gmail.com>
2021-06-15 16:30:55 -0700

    small type and locale function for looping access potentially

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,14 +2,14 @@ import english from './en_US.json';
 import lang from './languages.json';
 
 export type langType = {
-  code: string,
-  name: string
-}
+  code: string;
+  name: string;
+};
 
 export type langCodes = {
-  formatted: string,
-  code: string
-}
+  formatted: string;
+  code: string;
+};
 
 function localization() {
   // TODO flesh out if we localize
@@ -17,6 +17,23 @@ function localization() {
 }
 
 export const localeStrings = localization();
+
+// use dot notation
+export function locale(key: string): string {
+  const steps = key.split('.');
+  const length = steps.length;
+
+  let value: any | string = localeStrings;
+  for (let i = 0; i < length; i++) {
+    value = value[steps[i]];
+  }
+
+  if (typeof value !== 'string') {
+    return 'NO LOCALIZATION FOUND';
+  }
+
+  return value;
+}
 
 export const languageCodes: Array<langCodes> = lang.map((it: langType) => {
   return { formatted: it.name, code: it.code };
