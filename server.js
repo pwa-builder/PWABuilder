fe271f522f03ae092d84cfb8596374a42dd5c098
Justin Willis <jgw9617@gmail.com>
2021-06-25 11:01:41 -0700

    fix(): handle spa routing like azure static web apps did automatically

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const static = path.resolve(__dirname, 'dist');
+const index = path.resolve(static, 'index.html');
+
 app.use(cors());
-app.use(express.static("dist"));
+app.use(express.static(static));
+
+app.get('*', (req, res) => {
+  res.sendFile(index);
+});
 
 app.listen(port, () => {
   console.log("App Running");
