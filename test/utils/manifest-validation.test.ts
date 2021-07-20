77d828fcafbc9a49d32c2884ddd0861eb40ebd7a
lee-leonardo <lee.leonardo.king@gmail.com>
2021-07-20 11:35:23 -0700

    mocha rc works, these tests have esm and typescript compat issues...

---
+++ test/utils/manifest-validation.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+describe('Typescript usage suite', () => {
+  it('should be able to execute a test', () => {
+    expect(true);
+  });
+  it('should return expected string', () => {
+    expect(true);
+  });
+});
