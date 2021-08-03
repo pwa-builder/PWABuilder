89ac561d5398fc453668fd472896bffc80e1d563
lee-leonardo <lee.leonardo.king@gmail.com>
2021-08-03 09:44:36 -0700

    tmp commit

---
+++ unit_tests/services/app-info.karma.js
@@ -0,0 +1,7 @@
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+});
