9d58ecc408650637140359eadb834b3124a90887
Judah Gabriel Himango <judahgabriel@gmail.com>
2021-11-25 01:45:17 -0800

    Faster builds, faster loads from watch file change

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,9 +1,11 @@
 // This is the configuration for `npm dev` and `npm start`.
 
 export default {
-    appIndex: "build/index.html",
-    rootDir: "build/",
-    compatibility: "none",
-    watch: true,
-    open: "/"
-  };
+  appIndex: "build/index.html",
+  rootDir: "build/",
+  compatibility: "none",
+  watch: true,
+  watchExcludes: "*.js.map", // ignore js map changes
+  watchDebounce: 250,
+  open: "/"
+};
