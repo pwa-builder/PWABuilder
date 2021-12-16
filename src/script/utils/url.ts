e2543de4793dd8322b96afa82a863cc0f2db597d
Jaylyn Barbee <jaylyn.1b@gmail.com>
2021-12-15 19:04:47 -0500

    split the regex for validating a url up a bit for better readability.

--- src/script/utils/url.ts
+++ src/script/utils/url.ts
@@ -76,9 +76,13 @@ export function cleanUrl(url: string): string {
 }
 
 export function isValidURL(str: string) {
-  // from https://stackoverflow.com/a/5717133
+  // from https://stackoverflow.com/a/14582229 but removed the ip address section
   var pattern = new RegExp(
-    '^((https?:)?\\/\\/)?(?:\\S+(?::\\S*)?@)?(([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\\\#[-a-z\\\\d_]*)?',
+    '^((https?:)?\\/\\/)?' + // protocol
+    '(?:\\S+(?::\\S*)?@)?(([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}' + // domain name
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\\\#[-a-z\\\\d_]*)?', // fragment locator
     'i'
   );
   return !!pattern.test(str);
