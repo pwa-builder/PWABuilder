f1c5b15663101d4f1f72bfba946b965710c844aa
Jaylyn Barbee <jaylyn.1b@gmail.com>
2022-01-11 14:50:19 -0500

    put the timeout into the manifestDetector array so that if it resolves before the detectors we ignore them and create a manifest immediately.

--- src/script/services/manifest.ts
+++ src/script/services/manifest.ts
@@ -30,6 +30,8 @@ export let emptyManifest: Manifest = {
 async function getManifestViaPuppeteer(
   url: string
 ): Promise<ManifestDetectionResult> {
+  console.log("starting pup");
+  console.time("Pup");
   const encodedUrl = encodeURIComponent(url);
   const manifestTestUrl = `${env.api}/WebManifest?site=${encodedUrl}`;
   const response = await fetch(manifestTestUrl, {
@@ -40,6 +42,8 @@ async function getManifestViaPuppeteer(
       'Fetching manifest via Puppeteer service failed',
       response.statusText
     );
+    
+    console.timeEnd("Pup");
     throw new Error(
       `Unable to fetch response using ${manifestTestUrl}. Response status  ${response}`
     );
@@ -60,7 +64,7 @@ async function getManifestViaPuppeteer(
     console.info("Manifest detection via Puppeteer completed, but couldn't detect the manifest.", responseData);
     throw new Error("HTML parse manifest detector couldn't find the manifest. " + responseData.error);
   }
-
+  console.log("returning from pup");
   return {
     content: responseData.content.json,
     format: 'w3c',
@@ -81,6 +85,8 @@ async function getManifestViaPuppeteer(
 async function getManifestViaHtmlParse(
   url: string
 ): Promise<ManifestDetectionResult> {
+  console.log("starting html parse");
+  console.time("HTMLParse");
   const manifestTestUrl = `${env.manifestFinderUrl}?url=${encodeURIComponent(
     url
   )}`;
@@ -96,6 +102,7 @@ async function getManifestViaHtmlParse(
       'Fetching manifest via HTML parsing service failed due to no response data',
       response
     );
+    console.timeEnd("HTMLParse");
     throw new Error(responseData.error || "Manifest couldn't be fetched");
   }
 
@@ -108,7 +115,8 @@ async function getManifestViaHtmlParse(
     console.info("Manifest detection via HTML parse completed, but couldn't detect the manifest.", responseData);
     throw new Error("Manifest detection via HTML parsing completed but couldn't find the manifest. " + responseData.error);
   }
-
+  console.log("returning from html parse");
+  
   return {
     content: responseData.manifestContents,
     format: 'w3c',
@@ -158,17 +166,17 @@ async function fetchManifest(
       return;
     }
 
+    // Some sites that don't have a manifest take a long time for our Puppeteer-based test to complete.
+    // If 10 seconds passes, we ignore the detectors and move to creating a manifest in the interest of time. 
     const manifestDetectors = [
       getManifestViaPuppeteer(knownGoodUrl),
-      getManifestViaHtmlParse(knownGoodUrl)
+      getManifestViaHtmlParse(knownGoodUrl),
+      timeoutAfter(10000)
     ];
-    try {
-      // Timeout after 10 seconds.
-      // Some sites that don't have a manifest take a long time for our Puppeteer-based test to complete.
-      timeoutAfter(10000).then(() => reject("Timeout expired"));
 
-      const manifestDetectionResult = await Promise.any(manifestDetectors);
+    const manifestDetectionResult = await Promise.any(manifestDetectors);
 
+    if(manifestDetectionResult){
       const context = getManifestContext();
       if (!context.initialManifest) {
         initialManifest = manifestDetectionResult.content;
@@ -177,13 +185,18 @@ async function fetchManifest(
       }
 
       resolve(manifestDetectionResult);
-    } catch (manifestDetectionError) {
-      console.error('All manifest detectors failed.', manifestDetectionError);
+
+    } else {
+      console.log("in the else");
+      //clearTimeout(detectorTimeout);
+      //console.error('All manifest detectors failed.', manifestDetectionError);
+      console.time("else");
       const createdManifest = await createManifestFromPageOrEmpty(knownGoodUrl);
       const createdManifestResult = wrapManifestInDetectionResult(createdManifest, knownGoodUrl, true);
+      console.timeEnd("else");
       resolve(createdManifestResult);
     }
-  });
+});
 }
 
 /**
@@ -291,22 +304,23 @@ export function updateManifestEvent<T extends Partial<Manifest>>(detail: T) {
   });
 }
 
-function wrapManifestInDetectionResult(manifest: Manifest, url: string, generated: boolean): ManifestDetectionResult {
+async function wrapManifestInDetectionResult(manifest: Manifest, url: string, generated: boolean): ManifestDetectionResult {
   return {
-    content: manifest,
-    format: "w3c",
-    siteUrl: url,
-    generated: generated,
-    id: "",
-    generatedUrl: "",
-    default: {
-      short_name: manifest.short_name || "My PWA"
-    },
-    errors: [],
-    suggestions: [],
-    warnings: []
-  };
+      content: manifest,
+      format: "w3c",
+      siteUrl: url,
+      generated: generated,
+      id: "",
+      generatedUrl: "",
+      default: {
+        short_name: manifest.short_name || "My PWA"
+      },
+      errors: [],
+      suggestions: [],
+      warnings: []
+    }
 }
+
 
 type HtmlParseManifestFinderResult = {
   manifestUrl: string | null;
