337c1858fb01cb4b0a87989281a61957e319c1dd
Jaylyn Barbee <jaylyn.1b@gmail.com>
2022-03-09 16:59:48 -0500

    carousel working on 1366+ 100%. Need to look at smaller sizes and also infinite scroll

--- src/script/components/companies-packaged.ts
+++ src/script/components/companies-packaged.ts
@@ -1,6 +1,6 @@
 import { LitElement, css, html } from 'lit';
 
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 
 import {
   smallBreakPoint,
@@ -13,9 +13,17 @@ import {
 @customElement('companies-packaged')
 export class ComapniesPackaged extends LitElement {
 
+  @state() companies: string[] = ["facebook", "instagram", "mailchimp", "plutotv", "sketchapp", "tiktok", "twitter"] ;
+
   static get styles() {
     return [
     css`
+      :host {
+        --carousel-width: 1000px;
+        --slide-width: 200px;
+        --slide-height: 80px;
+        --carousel-image-width: 120px;
+      }
       #success-wrapper {
         display: flex;
         flex-direction: column;
@@ -55,27 +63,75 @@ export class ComapniesPackaged extends LitElement {
         background-position: center;
       }
 
+      @keyframes scroll {
+        0% { transform: translateX(0); }
+        25% { transform: translateX(calc(var(--slide-width) * -1)) }
+        50% { transform: translateX(calc(var(--slide-width) * -2)) }
+        75% { transform: translateX(calc(-var(--slide-width) * 1)) }
+        100% { transform: translateX(0); }
+      }
+
+      .slider {
+        background: white;
+        height: var(--slide-height);
+        overflow:hidden;
+        position: relative;
+        width: var(--carousel-width);
+      }
+
+      .slider::before,
+      .slider::after {
+        content: "";
+        height: 100px;
+        position: absolute;
+        width: 200px;
+        z-index: 2;
+      }
+      
+      .slider::after {
+        right: 0;
+        top: 0;
+        transform: rotateZ(180deg);
+      }
+
+      .slider::before {
+        left: 0;
+        top: 0;
+      }
+
+      .slide-track {
+        animation: scroll 15s infinite;
+        animation-delay: 3s;
+        display: flex;
+        width: calc(var(--slide-width) * 14);
+      }
+      
+      .slide {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: var(--slide-height);
+        width: var(--slide-width)
+      }
+
+      .slide img {
+        height: auto;
+        width: var(--carousel-image-width);
+      }
+
       /* < 480px */
       ${smallBreakPoint(css`
-          #success-wrapper #img-box {
-            background-image: url("/assets/new/packaged_320.png");
-            height: 7em;
-          }
+          /* 3 icons in the carousel */
       `)}
 
       /* 480px - 639px */
       ${mediumBreakPoint(css`
-          #success-wrapper #img-box {
-            background-image: url("/assets/new/packaged_480.png");
-            height: 7em;
-          }
+        /* 4 icons in the carousel */
       `)}
 
       /* 640px - 1023px */
       ${largeBreakPoint(css`
-        #success-wrapper #img-box {
-          background-size: 85%;
-        }
+          /* 5 icons in the carousel */
       `)}
 
       /*1024px - 1365px*/
@@ -85,9 +141,6 @@ export class ComapniesPackaged extends LitElement {
 
       /* > 1920px */
       ${xxxLargeBreakPoint(css`
-          #success-wrapper #img-box {
-            background-size: 50%;
-          }
       `)}
     `
     ];
@@ -98,6 +151,28 @@ export class ComapniesPackaged extends LitElement {
   }
 
   firstUpdated() {
+    const shuffled = this.shuffle(this.companies);
+    this.companies = [...shuffled];
+    console.log("companies", this.companies);
+  }
+
+  shuffle(array: any) {
+    let currentIndex = array.length
+    let randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
   }
 
 
@@ -108,7 +183,16 @@ export class ComapniesPackaged extends LitElement {
           <h2>Apps Packaged</h2>
           <p>Companies of all sizes—from startups to Fortune 500s—have used PWABuilder to package their PWAs.</p>
       </div>
-      <div id="img-box" alt="logos of companies that use PWA" style="visibility: hidden" ></div>
+      <div class="slider">
+        <div class="slide-track">
+          ${this.companies.map((comp: string) => 
+              html`
+              <div class="slide">
+                <img src="/assets/new/${comp}_carousel.png" alt="${comp} logo" />
+              </div>`
+            )}
+        </div>
+      </div>
     </div>
     `;
   }
