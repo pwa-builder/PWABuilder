4ad9e8c8f6198cd586d4c9a8e854dfa72bd4f57f
Jaylyn Barbee <jaylyn.1b@gmail.com>
2022-04-07 14:12:35 -0700

    start button fixed

--- src/script/components/loading-button.ts
+++ src/script/components/loading-button.ts
@@ -1,17 +1,17 @@
 import { LitElement, css, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { AppButtonElement } from '../utils/interfaces.components';
 
 import '../components/app-button';
-import { classMap } from 'lit/directives/class-map.js';
-
+import { FastButtonAppearance } from '../utils/fast-element';
 
 @customElement('loading-button')
-export class LoadingButton extends LitElement {
+export class LoadingButton extends LitElement implements AppButtonElement {
   @property({ type: String }) type = 'submit';
+  @property({ type: String }) colorMode = 'primary';
+  @property({ type: String }) appearance: FastButtonAppearance = 'neutral';
   @property({ type: Boolean }) loading = false;
   @property({ type: Boolean }) disabled = false;
-  @property({ type: Boolean }) secondary = false;
-  @property({ type: Boolean }) primary = false;
 
   static get styles() {
     return [
@@ -19,58 +19,16 @@ export class LoadingButton extends LitElement {
         :host {
           --loader-size: 1.8em;
         }
-
         fast-progress-ring {
           height: var(--loader-size);
           width: var(--loader-size);
-          margin: 0;
-
           --accent-foreground-rest: var(--secondary-color);
         }
-
-        #loading-options.secondary fast-progress-ring {
+        app-button.secondary fast-progress-ring {
           --accent-foreground-rest: var(--primary-color);
           --neutral-fill-rest: white;
           --neutral-fill-active: white;
-          --neutral-fill-hover: white;  
-        }
-
-        #loading-options.primary {
-          width: 100%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          padding: 0 35px;
-          font-size: 14px;
-          font-family: inherit;
-          font-weight: 700;
-          color: white;
-          background-color: #292c3a;
-          box-shadow: rgb(0 0 0 / 25%) 0px 1px 4px;
-          border-radius: 20px;
-        }
-
-        #loading-options.primary:hover {
-          background-color: #545454;
-        }
-
-        #loading-options.secondary {
-          display: flex;
-          justify-content: center;
-          padding: 10px 4px 8px 4px;
-          font-size: 14px;
-          font-family: inherit;
-          font-weight: 700;
-          color: #292c3a;
-          margin-top: 3px;
-          background-color: white;
-          box-shadow: rgb(0 0 0 / 25%) 0px 1px 4px;
-          border-radius: 20px;
-        }
-
-        #loading-options.secondary.disabled {
-          opacity: 0.8;
-          color: #747578;
+          --neutral-fill-hover: white;
         }
       `,
     ];
@@ -82,20 +40,18 @@ export class LoadingButton extends LitElement {
 
   render() {
     return html`
-      <div 
-        role="button"
-        aria-label="loading button"
-        id="loading-options"
-        class=${classMap({
-          secondary: this.secondary,
-          primary: this.primary,
-          disabled: this.disabled
-        })}
+      <app-button
+        class=${this.className}
+        part="underlying-button"
+        .appearance=${this.appearance}
+        .type=${this.type}
+        .colorMode=${this.colorMode}
+        ?disabled=${this.disabled || this.loading}
       >
         ${this.loading
           ? html`<fast-progress-ring></fast-progress-ring>`
           : html`<slot></slot>`}
-      </div>
+      </app-button>
     `;
   }
 }
