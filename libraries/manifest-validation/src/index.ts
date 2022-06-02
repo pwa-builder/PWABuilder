98f9cc20503b5dc10bea11ace2cc4618eda36e35
jgw96 <jgw9617@gmail.com>
2022-06-02 14:20:54 -0700

    chore(): add latest mani-validation lib

--- libraries/manifest-validation/src/index.ts
+++ libraries/manifest-validation/src/index.ts
@@ -1,16 +1,30 @@
 import { Manifest, Validation } from "./interfaces";
+import { isValidJSON } from "./utils/validation-utils";
 import { maniTests } from "./validations";
 
 export async function validateManifest(manifest: Manifest): Promise<Validation[]> {
     const validationErrors: Validation[] = [];
+    console.log('validating manifest', manifest);
+
+    const validJSON = isValidJSON(manifest);
+    
+    if (validJSON === false) {
+        throw new Error('Manifest is not valid JSON');
+    }
 
     Object.keys(manifest).forEach(async (key) => {
         for await (const test of maniTests) {
             if (test.member === key && test.test) {
                 const testResult = await test.test(manifest[key]);
 
+
                 if (testResult === false) {
-                    validationErrors.push(test)
+                    test.valid = false;
+                    validationErrors.push(test);
+                }
+                else {
+                    test.valid = true;
+                    validationErrors.push(test);
                 }
             }
         }
@@ -24,12 +38,7 @@ export async function validateSingleField(field: string, value: any): Promise<Va
         if (test.member === field && test.test) {
             const testResult = await test.test(value);
 
-            if (testResult === false) {
-                return test;
-            }
-            else {
-                return true
-            }
+            return testResult;
         }
     }
 
@@ -39,13 +48,59 @@ export async function validateSingleField(field: string, value: any): Promise<Va
 export async function validateRequiredFields(manifest: Manifest): Promise<Validation[]> {
     const requiredValidationErrors: Validation[] = [];
 
+    const validJSON = isValidJSON(manifest);
+    
+    if (validJSON === false) {
+        throw new Error('Manifest is not valid JSON');
+    }
+
     for await (const test of maniTests) {
-        if (test.category === "required") {
+        if (test && test.category === "required" && test.test) {
             if (Object.keys(manifest).includes(test.member) === false) {
-                requiredValidationErrors.push(test);
+                const testResult = await test.test(manifest[test.member]);
+
+                if (testResult === false) {
+                  requiredValidationErrors.push(test);
+                }
             }
         }
     }
 
     return requiredValidationErrors;
+}
+
+export async function validateImprovements(manifest: Manifest): Promise<Validation[]> {
+    const optionalValidationErrors: Validation[] = [];
+
+    const validJSON = isValidJSON(manifest);
+    
+    if (validJSON === false) {
+        throw new Error('Manifest is not valid JSON');
+    }
+
+    for await (const test of maniTests) {
+        if (test && test.category === "optional" && test.test) {
+            if (Object.keys(manifest).includes(test.member) === true) {
+                const testResult = await test.test(manifest[test.member]);
+
+                if (testResult === false) {
+                    optionalValidationErrors.push(test);
+                }
+            }
+        }
+    }
+
+    return optionalValidationErrors;
+}
+
+export async function isInstallReady(manifest: Manifest): Promise<boolean> {
+    const validJSON = isValidJSON(manifest);
+    
+    if (validJSON === false) {
+        throw new Error('Manifest is not valid JSON');
+    }
+    
+    const validations = await validateRequiredFields(manifest);
+
+    return validations.length === 0;
 }
--- libraries/manifest-validation/src/interfaces.ts
+++ libraries/manifest-validation/src/interfaces.ts
@@ -1,115 +1,116 @@
 export interface Validation {
-    infoString?: string;
-    category: string;
-    member: string;
-    defaultValue?: string | any[];
-    docsLink?: string;
-    errorString?: string;
-    quickFix: boolean;
-    test?: Function;
-    testName?: string;
+  infoString?: string;
+  category: string;
+  member: string;
+  valid?: boolean;
+  defaultValue?: string | any[];
+  docsLink?: string;
+  errorString?: string;
+  quickFix: boolean;
+  test?: Function;
+  testName?: string;
 }
 
 export interface Manifest {
-    background_color?: string;
-    description?: string;
-    dir?: "auto" | "ltr" | "rtl" | string;
-    display?: "standalone" | "fullscreen" | "fullscreen-sticky";
-    lang?: string | undefined;
-    name?: string | undefined;
-    orientation?:
-      | "any"
-      | "natural"
-      | "landscape"
-      | "portrait"
-      | "portrait-primary"
-      | "portrait-secondary"
-      | "landscape-primary"
-      | "landscape-secondary";
-    prefer_related_applications?: boolean;
-    related_applications?: RelatedApplication[];
-    scope?: string;
-    short_name?: string;
-    start_url?: string;
-    theme_color?: string;
-    generated?: boolean;
-    shortcuts?: ShortcutItem[];
-    categories?: string[];
-    screenshots?: Screenshot[];
-    iarc_rating_id?: string;
-    iconBlobUrls?: string[];
-    icons?: Icon[];
-    share_target?: ShareTarget;
-  
-    // for custom properties as well as using object notations: manifest[key]
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore - accomodate custom entries... these can be a pain
-    [key: string]: string | boolean | undefined | Array<any> | any;
-  }
-  
-  export interface ShortcutItem {
-    name: string;
-    url: string;
-    description?: string;
-    short_name?: string;
-    icons?: Icon[];
-  }
-  
-  export interface Icon {
-    src: string;
-    generated?: boolean;
-    type?: string;
-    sizes?: string;
-    purpose?: "any" | "maskable" | "monochrome";
-    label?: string;
-  }
-  
-  export interface Screenshot extends Icon {
-    platform?:
-      | "narrow"
-      | "wide"
-      | "android"
-      | "chromeos"
-      | "ios"
-      | "kaios"
-      | "macos"
-      | "windows"
-      | "xbox"
-      | "chrome_web_store"
-      | "play"
-      | "itunes"
-      | "microsoft-inbox"
-      | "microsoft-store";
-  }
-  
-  export interface RelatedApplication {
-    platform: string;
-    url?: string | null;
-    id?: string | null;
-    min_version?: string | null;
-    fingerprints?: Fingerprint[];
-  }
-  
-  export interface Fingerprint {
-    type: string;
-    value: string;
-  }
-  
-  export interface ShareTarget {
-    action?: string;
-    method?: string;
-    enctype?: string;
-    params?: ShareTargetParams;
-  }
-  
-  export interface ShareTargetParams {
-    title?: string;
-    text?: string;
-    url?: string;
-    files?: FilesParams[];
-  }
-  
-  export interface FilesParams {
-    name: string;
-    accept: string[];
-  }
+  background_color?: string;
+  description?: string;
+  dir?: "auto" | "ltr" | "rtl" | string;
+  display?: "standalone" | "fullscreen" | "fullscreen-sticky";
+  lang?: string | undefined;
+  name?: string | undefined;
+  orientation?:
+    | "any"
+    | "natural"
+    | "landscape"
+    | "portrait"
+    | "portrait-primary"
+    | "portrait-secondary"
+    | "landscape-primary"
+    | "landscape-secondary";
+  prefer_related_applications?: boolean;
+  related_applications?: RelatedApplication[];
+  scope?: string;
+  short_name?: string;
+  start_url?: string;
+  theme_color?: string;
+  generated?: boolean;
+  shortcuts?: ShortcutItem[];
+  categories?: string[];
+  screenshots?: Screenshot[];
+  iarc_rating_id?: string;
+  iconBlobUrls?: string[];
+  icons?: Icon[];
+  share_target?: ShareTarget;
+
+  // for custom properties as well as using object notations: manifest[key]
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - accomodate custom entries... these can be a pain
+  [key: string]: string | boolean | undefined | Array<any> | any;
+}
+
+export interface ShortcutItem {
+  name: string;
+  url: string;
+  description?: string;
+  short_name?: string;
+  icons?: Icon[];
+}
+
+export interface Icon {
+  src: string;
+  generated?: boolean;
+  type?: string;
+  sizes?: string;
+  purpose?: "any" | "maskable" | "monochrome";
+  label?: string;
+}
+
+export interface Screenshot extends Icon {
+  platform?:
+    | "narrow"
+    | "wide"
+    | "android"
+    | "chromeos"
+    | "ios"
+    | "kaios"
+    | "macos"
+    | "windows"
+    | "xbox"
+    | "chrome_web_store"
+    | "play"
+    | "itunes"
+    | "microsoft-inbox"
+    | "microsoft-store";
+}
+
+export interface RelatedApplication {
+  platform: string;
+  url?: string | null;
+  id?: string | null;
+  min_version?: string | null;
+  fingerprints?: Fingerprint[];
+}
+
+export interface Fingerprint {
+  type: string;
+  value: string;
+}
+
+export interface ShareTarget {
+  action?: string;
+  method?: string;
+  enctype?: string;
+  params?: ShareTargetParams;
+}
+
+export interface ShareTargetParams {
+  title?: string;
+  text?: string;
+  url?: string;
+  files?: FilesParams[];
+}
+
+export interface FilesParams {
+  name: string;
+  accept: string[];
+}
