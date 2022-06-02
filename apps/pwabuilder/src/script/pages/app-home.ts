d44dfbb12bc339201c1a27934c29cc8e2f255cbd
Jaylyn Barbee <jaylyn.1b@gmail.com>
2022-06-02 16:10:49 -0700

    working on animating mani

--- apps/pwabuilder/src/script/pages/app-home.ts
+++ apps/pwabuilder/src/script/pages/app-home.ts
@@ -50,7 +50,7 @@ export class AppHome extends LitElement {
           content: "";
         }
         #home-block {
-          background: url(/assets/new/Hero1920_withmani.jpg);
+          background: url(/assets/new/NoOtterHeroBackground_1920.png);
           background-position: center center;
           background-size: cover;
           background-repeat: no-repeat;
@@ -60,6 +60,11 @@ export class AppHome extends LitElement {
           align-items: center;
           padding: 4em;
         }
+        
+        #mani {
+          position: fixed;
+        }
+
         #wrapper {
           width: 1000px;
         }
@@ -220,7 +225,7 @@ export class AppHome extends LitElement {
         ${largeBreakPoint(css`
           #home-block {
             padding-left: 4.5em;
-            background: url(/assets/new/Hero1024_withmani.png);
+            background: url(/assets/new/NoOtterHeroBackground_1024.png);
             background-position: center center;
             background-size: cover;
             background-repeat: no-repeat;
@@ -349,7 +354,7 @@ export class AppHome extends LitElement {
         /*1024px - 1365px*/ 
         ${xLargeBreakPoint(css`
             #home-block {
-              background: url(/assets/new/Hero1366_withmani.png);
+              background: url(/assets/new/NoOtterHeroBackground_1366.png);
               background-position: center center;
               background-size: cover;
               background-repeat: no-repeat;
@@ -499,6 +504,7 @@ export class AppHome extends LitElement {
       <app-header part="header"></app-header>
       <main>
         <div id="home-block">
+          <img id="mani" src="/assets/new/Otter.svg" alt="mani the web otter" />
           <div id="wrapper">
             <h1 id="home-header" slot="hero-container">
               Helping developers build and publish PWAs
