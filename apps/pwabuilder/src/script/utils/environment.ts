d4af7f5988870a28a28942e4d147b7dc364e362a
Amrutha Srinivasan <amrutha.srinivasan95@gmail.com>
2022-06-16 14:08:43 -0700

    Testing on dev with dev slot android packaging

--- apps/pwabuilder/src/script/utils/environment.ts
+++ apps/pwabuilder/src/script/utils/environment.ts
@@ -28,7 +28,7 @@ if ((window as any).ENV == 'production') {
   env.windowsPackageGeneratorUrl =
     'https://pwabuilder-winserver.centralus.cloudapp.azure.com/msix/generatezip';
   env.androidPackageGeneratorUrl =
-    'https://pwabuilder-cloudapk.azurewebsites.net';
+    'https://pwabuilder-cloudapk-pre.azurewebsites.net';
   env.iosPackageGeneratorUrl =
     'https://pwabuilder-ios.azurewebsites.net/packages/create';
   env.oculusPackageGeneratorUrl =
