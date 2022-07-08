1f234aa835690e60e310008163385df6edd0a1d8
Amrutha Srinivasan <amsrin@microsoft.com>
2022-07-08 14:27:31 -0700

    msal2.0 to pwa-auth

--- components/pwa-auth/src/signin-provider.ts
+++ components/pwa-auth/src/signin-provider.ts
@@ -1,13 +1,17 @@
-import { SignInResult } from "./signin-result";
+import { SignInResult } from './signin-result';
 
 export interface SignInProvider {
-    /** 
-     * Kicks off the OAuth sign-in flow.
-     */
-    signIn(): Promise<SignInResult>;
+  /**
+   * Kicks off the OAuth sign-in flow.
+   */
+  signIn(): Promise<SignInResult>;
 
-    /** 
-     * Loads any 3rd party dependencies used by the provider. For example, the Facebook provider may load the Facebook SDK if it's not already loaded.
-     */
-    loadDependencies(): Promise<void>;
+  /**
+   * Loads any 3rd party dependencies used by the provider. For example, the Facebook provider may load the Facebook SDK if it's not already loaded.
+   */
+  loadDependencies(): Promise<void>;
+
+  signOut?(): Promise<void> | undefined;
+
+  // getAccessTokenSilent?();
 }
