e996c6e42bfb215b8b2bc75de9b2897a86e8d0c4
Jaylyn Barbee <jaylyn.1b@gmail.com>
2022-07-21 16:38:43 -0700

    Small variable change

--- apps/pwabuilder/src/script/pages/app-report.ts
+++ apps/pwabuilder/src/script/pages/app-report.ts
@@ -108,7 +108,7 @@ export class AppReport extends LitElement {
 
   // will be used to control the state of the "Package for store" button.
   @state() canPackageList: boolean[] = [];
-  @state() canPackage: boolean = true;
+  @state() canPackage: boolean = false;
   @state() manifestEditorOpened: boolean = false;
   @state() publishModalOpened: boolean = false;
 
