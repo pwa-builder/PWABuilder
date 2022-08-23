ad3d49e9555f70f840167564549eed3c543f346a
Justin Willis (HE / HIM) <juwillis@microsoft.com>
2022-08-23 11:47:37 -0700

    chore(vscode): service worker docs

--- apps/pwabuilder-vscode/src/services/service-workers/simple-service-worker.ts
+++ apps/pwabuilder-vscode/src/services/service-workers/simple-service-worker.ts
@@ -7,6 +7,22 @@ let existingWorker: vscode.Uri | undefined = undefined;
 const simple_worker = `
     // Based off of https://github.com/pwa-builder/PWABuilder/blob/main/docs/sw.js
 
+    /*
+      Welcome to our basic Service Worker! This Service Worker offers a basic offline experience
+      while also being easily customizeable. You can add in your own code to implement the capabilities
+      listed below, or change anything else you would like.
+
+
+      Need an introduction to Service Workers? Check our docs here: https://docs.pwabuilder.com/#/home/sw-intro
+      Want to learn more about how our Service Worker generation works? Check our docs here: https://docs.pwabuilder.com/#/studio/existing-app?id=add-a-service-worker
+
+      Did you know that Service Workers offer many more capabilities than just offline? 
+        - Background Sync: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/06
+        - Periodic Background Sync: https://web.dev/periodic-background-sync/
+        - Push Notifications: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=push-notifications-on-the-web
+        - Badges: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=application-badges
+    */
+
     const HOSTNAME_WHITELIST = [
         self.location.hostname,
         'fonts.gstatic.com',
