f71f2b30cead8c81ebde90692ff94d92956de552
Jaylyn Barbee <jaylyn.1b@gmail.com>
2022-11-29 13:22:05 -0500

    Categories was missing isArray check

--- libraries/manifest-validation/src/validations.ts
+++ libraries/manifest-validation/src/validations.ts
@@ -427,12 +427,16 @@ export const maniTests: Array<Validation> = [
             "https://docs.pwabuilder.com/#/builder/manifest?id=categories-array",
         quickFix: true,
         test: (value: any[]) => {
-            if (value) {
-                const isGood = containsStandardCategory(value);
-                return isGood;
+            let isGood;
+            if(value){
+                containsStandardCategory(value) && Array.isArray(value) 
+                ? 
+                isGood = true 
+                : 
+                isGood = false;
             }
 
-            return false;
+            return isGood
         },
         errorString: "categories should be a non-empty array"
     },
