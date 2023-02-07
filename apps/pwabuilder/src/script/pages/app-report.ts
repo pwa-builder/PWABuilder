6ba50e656705b3039c15579111277c25f386edf3
Jaylyn Barbee <51131738+Jaylyn-Barbee@users.noreply.github.com>
2023-02-07 13:39:19 -0500

    Testing fix for action items pane resizing on paging. (#3828)

--- apps/pwabuilder/src/script/pages/app-report.ts
+++ apps/pwabuilder/src/script/pages/app-report.ts
@@ -646,6 +646,9 @@ export class AppReport extends LitElement {
           font-size: var(--subheader-font-size);
         }
 
+        .todo-items-holder {
+        }
+
         #pagination-actions {
           display: flex;
           align-items: center;
@@ -1057,6 +1060,7 @@ export class AppReport extends LitElement {
           color: var(--primary-color);
           box-sizing: border-box;
           animation: animloader 2s linear infinite;
+          grid-row: 3;
         }
 
         @keyframes animloader {
@@ -1396,7 +1400,7 @@ export class AppReport extends LitElement {
         siteName: parsedManifestContext.manifest.short_name
           ? parsedManifestContext.manifest.short_name
           : (parsedManifestContext.manifest.name ? parsedManifestContext.manifest.name : 'Untitled App'),
-        siteUrl: cleanURL.replace(/\/$/, ""),
+        siteUrl: cleanURL,
         iconURL: iconUrl,
         iconAlt: "Your sites logo",
         description: parsedManifestContext.manifest.description
@@ -1416,7 +1420,7 @@ export class AppReport extends LitElement {
     } else {
         this.appCard = {
           siteName: "Missing Name",
-          siteUrl: cleanURL.replace(/\/$/, ""),
+          siteUrl: cleanURL,
           description: "Your manifest description is missing.",
           iconURL: "/assets/new/icon_placeholder.png",
           iconAlt: "A placeholder for you sites icon"
@@ -1975,7 +1979,20 @@ export class AppReport extends LitElement {
   // Pages the action items
   paginate() {
     let array = this.sortTodos();
-    return array.slice((this.pageNumber - 1) * this.pageSize, this.pageNumber * this.pageSize);
+    let itemsOnPage = array.slice((this.pageNumber - 1) * this.pageSize, this.pageNumber * this.pageSize);
+
+    let holder = (this.shadowRoot?.querySelector(".todo-items-holder") as HTMLElement);
+    if(itemsOnPage.length < this.pageSize && this.pageNumber == 1){
+      holder.style.display = 'flex';
+      holder.style.flexDirection = 'column';
+      holder.style.gridTemplateRows = 'unset';
+    } else {
+      holder.style.display = 'grid';
+      holder.style.gridTemplateRows = 'repeat(5, 1fr)';
+      holder.style.flexDirection = 'unset';
+    }
+
+    return itemsOnPage;
   }
 
   // Moves to the next window in the action items list
@@ -2147,18 +2164,19 @@ export class AppReport extends LitElement {
                   <img class="dropdown_icon" data-card="todo" src="/assets/new/dropdownIcon.svg" alt="dropdown toggler"/>
                 
               </div>
-             ${(!this.manifestDataLoading && !this.swDataLoading && !this.secDataLoading) ? this.paginate().map((todo: any) =>
-                html`
-                  <todo-item
-                    .status=${todo.status}
-                    .field=${todo.field}
-                    .fix=${todo.fix}
-                    .card=${todo.card}
-                    .displayString=${todo.displayString}
-                    @todo-clicked=${(e: CustomEvent) => this.animateItem(e)}>
-                  </todo-item>`
-              ) : html`<span class="loader"></span>`}
-
+              <div class="todo-items-holder">
+                ${(!this.manifestDataLoading && !this.swDataLoading && !this.secDataLoading) ? this.paginate().map((todo: any) =>
+                    html`
+                      <todo-item
+                        .status=${todo.status}
+                        .field=${todo.field}
+                        .fix=${todo.fix}
+                        .card=${todo.card}
+                        .displayString=${todo.displayString}
+                        @todo-clicked=${(e: CustomEvent) => this.animateItem(e)}>
+                      </todo-item>`
+                  ) : html`<span class="loader"></span>`}
+              </div>
             ${((!this.manifestDataLoading && !this.swDataLoading && !this.secDataLoading) && (this.todoItems.length > this.pageSize)) ?
               html`
               <div id="pagination-actions">
