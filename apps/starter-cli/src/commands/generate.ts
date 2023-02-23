55490e9cb7951311b6d4001292ee3466d38dfa4f
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2023-02-23 13:20:05 -0800

    Added clack prompts and implemented for create command

--- apps/starter-cli/src/commands/generate.ts
+++ apps/starter-cli/src/commands/generate.ts
@@ -1,40 +1,65 @@
-import type { Arguments, CommandBuilder } from "yargs";
+import type { Arguments, CommandBuilder, string } from "yargs";
 import type { GenerateCommandOptions } from "../types/command-options-types";
+import * as prompts from "@clack/prompts";
 
 import { litFileReplaceList } from "../util/replace-lists";
 import { replaceInFileList, removeDirectory } from "../util/file-util";
 import { execSyncWrapper } from "../util/util";
 
-export const command: string = 'generate [name]';
+export const command: string = 'create [name]';
 export const desc: string = '';
-
-const litTemplateRepoString: string = "https://github.com/pwa-builder/pwa-starter.git";
-const basicLitTemplateRepoString: string = "https://github.com/pwa-builder/pwa-starter.git";
 const defaultName: string = "pwa-starter";
 
+const templateToRepoURLMap = {
+  'default': "https://github.com/pwa-builder/pwa-starter.git",
+  'basic': "https://github.com/pwa-builder/pwa-starter-basic.git"
+};
+
 export const builder: CommandBuilder<GenerateCommandOptions, GenerateCommandOptions> = (yargs) =>
-  yargs
-    .options({
-      basic: { type: 'boolean'}
+  yargs.options({
+      template: { type: 'string', alias: 't'}
     })
     .positional('name', {type: "string", demandOption: false});
+    
 
-export const handler = (argv: Arguments<GenerateCommandOptions>): void => {
-  const {name, basic} = argv;
-  const repoName: string = name ? name : defaultName;
+export const handler = async (argv: Arguments<GenerateCommandOptions>): Promise<void> => {
+  const {name, template} = await promptForMissingArgs(argv);
 
-  cloneRepoBasedOnFlag(basic, repoName);
-  removeGit(repoName);
-  gitInit(repoName);
-  npmInit(repoName);
+  cloneRepoBasedOnTemplate(template, name);
+  removeGit(name);
+  gitInit(name);
+  npmInit(name);
 
-  if(repoName != defaultName) {
-    setNewName(repoName, litFileReplaceList);
+  if(name != defaultName) {
+    setNewName(name, litFileReplaceList);
   }
 };
 
+async function promptForMissingArgs(argv: Arguments<GenerateCommandOptions>): Promise<{name: string, template: string}> {
+  const {name, template} = argv;
+  let finalOptions = {name: defaultName, template: 'default'};
+  if(!name) {
+    finalOptions.name = await prompts.text({
+      message: "Enter a name for your new PWA: ",
+      placeholder: defaultName,
+      initialValue: defaultName
+    }) as string;
+  }
+  if(!template) {
+    finalOptions.template = await prompts.select({
+      message: 'Select a template for your PWA:',
+      options: [
+        {value: "default", label: "Classic PWA Starter", hint: "Recommended"},
+        {value: "basic", label: "Simplified PWA Starter", hint: "Less dependencies and a vanilla JS service worker."}
+      ]
+    }) as string
+  }
+
+  return finalOptions;
+}
+
 function clone(repoUrl: string, name: string) {
-  const cloneCommand: string = 'git clone -b cli-dev ' + repoUrl + ' ' + name;
+  const cloneCommand: string = 'git clone ' + repoUrl + ' ' + name;
   execSyncWrapper(cloneCommand);
 }
 
@@ -48,12 +73,11 @@ function npmInit(directory: string) {
   execSyncWrapper(npmInitCommand, directory);
 }
 
-function cloneRepoBasedOnFlag(flag : boolean | undefined, name: string) {
-  if ( flag ) {
-    clone(basicLitTemplateRepoString, name);
-  }
-  else {
-    clone(litTemplateRepoString, name);
+function cloneRepoBasedOnTemplate(template : string | undefined, name: string) {
+  if(template) {
+    clone(templateToRepoURLMap[template], name);
+  } else {
+    clone(templateToRepoURLMap['default'], name);
   }
 }
 
