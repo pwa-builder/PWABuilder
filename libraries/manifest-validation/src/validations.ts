571067adda6fbd47b256cd109bf41d7f1da17b42
Justin Willis <jgw9617@gmail.com>
2023-03-07 11:17:43 -0800

    [Validations] Categories fix (#3889)
    fixes #[issue number]
    https://github.com/pwa-builder/PWABuilder/issues/3866
    
    ## PR Type
    <!-- Please uncomment one ore more that apply to this PR -->
    
    Bugfix
    <!-- - Feature -->
    <!-- - Code style update (formatting) -->
    <!-- - Refactoring (no functional changes, no api changes) -->
    <!-- - Build or CI related changes -->
    <!-- - Documentation content changes -->
    <!-- - Sample app changes -->
    <!-- - Other... Please describe: -->
    
    
    ## Describe the current behavior?
    <!-- Please describe the current behavior that is being modified or link
    to a relevant issue. -->
    We were testing for A) that categories is an array and B) that it was a
    "standard category"
    
    ## Describe the new behavior?
    We no longer test that each category is a "standard" category because:
    A) The criteria for the list of categories on the W3C spec is unclear,
    not used by any browser, and not used by any store.
    B) Start moving towards each test only testing one thing, of which
    categories is one of the last
    
    ## PR Checklist
    
    - [x ] Test: run `npm run test` and ensure that all tests pass
    - [x ] Target main branch (or an appropriate release branch if
    appropriate for a bug fix)
    - [ x] Ensure that your contribution follows [standard accessibility
    guidelines](https://docs.microsoft.com/en-us/microsoft-edge/accessibility/design).
    Use tools like https://webhint.io/ to validate your changes.
    
    
    ## Additional Information
    
    ---------
    
    Co-authored-by: Justin Willis (HE / HIM) <juwillis@microsoft.com>

--- libraries/manifest-validation/src/validations.ts
+++ libraries/manifest-validation/src/validations.ts
@@ -400,8 +400,8 @@ export const maniTests: Array<Validation> = [
         errorString: "shortcuts should not include webp images",
         quickFix: true,
         test: (value: any) => {
-            if(value && value.length === 0) return true;
-            if(value.icons && value.icons.length === 0) return true;
+            if (value && value.length === 0) return true;
+            if (value.icons && value.icons.length === 0) return true;
             const isArray = value && Array.isArray(value);
             if (isArray) {
 
@@ -410,7 +410,7 @@ export const maniTests: Array<Validation> = [
                 at least one webp image somewhere in their shortcuts. */
                 const noWebp = value.every((shortcut) => {
                     // If there are no icons, then it cannot contain webp.
-                    if(!shortcut.icons) return true;
+                    if (!shortcut.icons) return true;
                     // this returns TRUE if every icon in the shortcut does not have webp.
                     return shortcut.icons!.every((icon: Icon) => {
                         return icon.type !== "image/webp";
@@ -432,13 +432,13 @@ export const maniTests: Array<Validation> = [
         errorString: "One or more of your shortcuts has icons but does not have one with size 96x96",
         quickFix: false,
         test: (value: any[]) => {
-            if(value && value.length === 0) return true;
+            if (value && value.length === 0) return true;
             const isArray = value && Array.isArray(value);
             if (isArray) {
                 /* we use every here bc every shortcut needs at 
                 least one icon with size 96x96 no  icons at all */
                 const has96x96Icon = value.every((shortcut) => {
-                    if(!shortcut.icons) return true;
+                    if (!shortcut.icons) return true;
                     // we use some here bc only one icon has to be that size
                     return shortcut.icons!.some((icon: Icon) => {
                         return icon.sizes === "96x96";
@@ -477,7 +477,7 @@ export const maniTests: Array<Validation> = [
         quickFix: true,
         test: (value: any[]) => {
             const isArray = value && Array.isArray(value);
-            if(value && value.length === 0) return true;
+            if (value && value.length === 0) return true;
             if (isArray) {
                 let passed = value.every((app: RelatedApplication) => {
                     const check = validateSingleRelatedApp(app);
@@ -519,7 +519,7 @@ export const maniTests: Array<Validation> = [
         test: (value: any[]) => {
             let isGood;
             if (value) {
-                containsStandardCategory(value) && Array.isArray(value)
+                Array.isArray(value)
                     ?
                     isGood = true
                     :
@@ -761,4 +761,4 @@ export async function findSingleField(field: string, value: any): Promise<single
 
         resolve({ "valid": singleField, "errors": failedTests });
     })
-  }
+}
