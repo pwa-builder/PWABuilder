07940886a0be2bd21c8ab3a09de78b85e945e1e4
jgw96 <jgw9617@gmail.com>
2023-03-27 13:45:49 -0700

    add test for android packaging

--- apps/pwabuilder/tests/packaging.spec.ts
+++ apps/pwabuilder/tests/packaging.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 // only run this test once
-test('Ensure demo app can be packaged for windows', async ({ page }) => {
+test('Ensure demo app can be packaged for Windows', async ({ page }) => {
     test.slow();
 
     const demoButton = page.locator('id=demo-action');
@@ -53,6 +53,52 @@ test('Ensure demo app can be packaged for windows', async ({ page }) => {
     const request = await pageRequest;
 
     // wait for response to https://pwabuilder-winserver.centralus.cloudapp.azure.com/msix/generatezip to finish
+
+    const response = await request.response();
+
+    if (response) {
+        expect(response.status()).toBe(200);
+    }
+    else {
+        throw new Error('Response was undefined');
+    }
+})
+
+test('Ensure demo app can be packaged for Android', async ({ page }) => {
+    test.slow();
+    test.setTimeout(120000)
+
+    const demoButton = page.locator('id=demo-action');
+
+    // click demo button to start new test
+    await demoButton.click();
+
+    // wait for network to be done
+    await page.waitForLoadState('networkidle');
+
+    // our url should contain /reportcard
+    await expect(page.url()).toContain('/reportcard');
+
+    // wait for tests to end
+    await page.waitForLoadState('networkidle');
+
+    const packageButton = page.locator('id=pfs');
+
+    // click package button
+    await packageButton.click();
+
+    const androidPackageButton = page.locator('id=android-package-button');
+
+    // click windows package button
+    await androidPackageButton.click();
+
+    const generateButton = page.locator('id=generate-submit');
+
+    await expect(generateButton).toBeVisible();
+
+    const pageRequest = page.waitForRequest('https://pwabuilder-cloudapk.azurewebsites.net/generateAppPackage');
+    await generateButton.click();
+    const request = await pageRequest;
 
     const response = await request.response();
 
