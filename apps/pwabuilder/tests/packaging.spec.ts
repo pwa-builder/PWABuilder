101758dd87dcce086b49732a8dbc71c9f189bab1
Justin Willis <jgw9617@gmail.com>
2023-04-04 11:48:12 -0700

    Testing with Playwright v0.5 (#3932)
    fixes #3931 
    <!-- Link to relevant issue (for ex: "fixes #1234") which will
    automatically close the issue once the PR is merged -->
    
    ## PR Type
    <!-- Please uncomment one ore more that apply to this PR -->
    Bugfix
    <!-- - Feature -->
    <!-- - Code style update (formatting) -->
    <!-- - Refactoring (no functional changes, no api changes) -->
    Build or CI related changes
    <!-- - Documentation content changes -->
    <!-- - Sample app changes -->
    <!-- - Other... Please describe: -->
    
    
    ## Describe the current behavior?
    <!-- Please describe the current behavior that is being modified or link
    to a relevant issue. -->
    No automated testing
    
    ## Describe the new behavior?
    With this PR, we now have an automated testing setup that will run on
    YOUR LOCAL CODE, or, in Github Actions (not added as part of this PR,
    coming in next PR to keep things targeted) on the site with the changes
    from that PR. Run `npm run test` to start, you may get a prompt to run
    `npx playwright install` if you don't have the right dependencies.
    
    ## PR Checklist
    
    - [x ] Test: run `npm run test` and ensure that all tests pass
    - [x ] Target main branch (or an appropriate release branch if
    appropriate for a bug fix)
    - [x ] Ensure that your contribution follows [standard accessibility
    guidelines](https://docs.microsoft.com/en-us/microsoft-edge/accessibility/design).
    Use tools like https://webhint.io/ to validate your changes.
    
    
    ## Additional Information

---
+++ apps/pwabuilder/tests/packaging.spec.ts
@@ -0,0 +1,111 @@
+import { test, expect } from '@playwright/test';
+
+const url = 'http://localhost:3000';
+
+// before each test
+test.beforeEach(async ({ page }) => {
+    await page.goto(url);
+});
+
+// only run this test once
+test('Ensure demo app can be packaged for Windows', async ({ page }) => {
+    test.slow();
+
+    const demoButton = page.locator('id=demo-action');
+
+    // click demo button to start new test
+    await demoButton.click();
+
+    // wait for network to be done
+    await page.waitForLoadState('networkidle');
+
+    // our url should contain /reportcard
+    await expect(page.url()).toContain('/reportcard');
+
+    // wait for tests to end
+    await page.waitForLoadState('networkidle');
+
+    const packageButton = page.locator('id=pfs');
+
+    // click package button
+    await packageButton.click();
+
+    const windowsPackageButton = page.locator('id=windows-package-button');
+
+    // click windows package button
+    await windowsPackageButton.click();
+
+    // fill out form
+    await page.fill('id=package-id-input', 'com.webboard.pwa');
+
+    await page.fill('id=publisher-display-name-input', 'Contoso Inc.');
+
+    await page.fill('id=publisher-id-input', 'CN=asdfasdfasdflkjlkjhlkjh');
+
+    const generateButton = page.locator('id=generate-submit');
+    // await generateButton.click();
+
+    await expect(generateButton).toBeVisible();
+
+    // wait on request to https://pwabuilder-winserver.centralus.cloudapp.azure.com/msix/generatezip to finish
+    const pageRequest = page.waitForRequest('https://pwabuilder-winserver.centralus.cloudapp.azure.com/msix/generatezip');
+    await generateButton.click();
+    const request = await pageRequest;
+
+    // wait for response to https://pwabuilder-winserver.centralus.cloudapp.azure.com/msix/generatezip to finish
+
+    const response = await request.response();
+
+    if (response) {
+        expect(response.status()).toBe(200);
+    }
+    else {
+        throw new Error('Response was undefined');
+    }
+})
+
+test('Ensure demo app can be packaged for Android', async ({ page }) => {
+    test.slow();
+    test.setTimeout(120000)
+
+    const demoButton = page.locator('id=demo-action');
+
+    // click demo button to start new test
+    await demoButton.click();
+
+    // wait for network to be done
+    await page.waitForLoadState('networkidle');
+
+    // our url should contain /reportcard
+    await expect(page.url()).toContain('/reportcard');
+
+    // wait for tests to end
+    await page.waitForLoadState('networkidle');
+
+    const packageButton = page.locator('id=pfs');
+
+    // click package button
+    await packageButton.click();
+
+    const androidPackageButton = page.locator('id=android-package-button');
+
+    // click windows package button
+    await androidPackageButton.click();
+
+    const generateButton = page.locator('id=generate-submit');
+
+    await expect(generateButton).toBeVisible();
+
+    const pageRequest = page.waitForRequest('https://pwabuilder-cloudapk.azurewebsites.net/generateAppPackage');
+    await generateButton.click();
+    const request = await pageRequest;
+
+    const response = await request.response();
+
+    if (response) {
+        expect(response.status()).toBe(200);
+    }
+    else {
+        throw new Error('Response was undefined');
+    }
+})
