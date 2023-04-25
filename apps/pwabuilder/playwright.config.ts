075e8214514ee4bb4bc70f915a431386503aaf74
jgw96 <jgw9617@gmail.com>
2023-04-25 12:39:09 -0700

    add bypassCSP option

--- apps/pwabuilder/playwright.config.ts
+++ apps/pwabuilder/playwright.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    bypassCSP: true
   },
 
   /* Configure projects for major browsers */
