7ad28f1113596bba426d443c56735e3ef2130230
Jaylyn Barbee <jaylyn.1b@gmail.com>
2023-07-18 12:46:41 -0400

    removed 96x96 requirement from shortcut validation

--- libraries/manifest-validation/src/mani-tests.ts
+++ libraries/manifest-validation/src/mani-tests.ts
@@ -488,7 +488,8 @@ export const maniTests: Array<Validation> = [
                 return false;
             }
 
-            /* we use every here bc every shortcut needs at 
+            // Commenting this block out, this shouldn't be an error but instead a warning.
+            /* /* we use every here bc every shortcut needs at 
                 least one icon with size 96x96 no  icons at all */
             const has96x96Icon = value.every((shortcut: {icons?: Icon[]}) => {
                 if (!shortcut.icons) return true;
@@ -500,7 +501,7 @@ export const maniTests: Array<Validation> = [
             if (!has96x96Icon) {
                 this.errorString = "One or more of your shortcuts has icons but does not have one with size 96x96";
                 return false;
-            }
+            } */
 
             return true;
         }
