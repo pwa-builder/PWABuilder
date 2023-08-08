207a9e4b31b4f789a72be7dec4ab7fff6a4d599b
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2023-08-08 12:53:47 +0200

    to prev

--- apps/pwabuilder/src/script/utils/android-validation.ts
+++ apps/pwabuilder/src/script/utils/android-validation.ts
@@ -60,7 +60,7 @@ export interface AndroidPackageOptions  extends PackageOptions {
   splashScreenFadeOutDuration: number;
   startUrl: string;
   themeColor: string;
-  themeColorDark: string;
+  themeColorDark?: string;
   webManifestUrl: string;
   fullScopeUrl: string;
   minSdkVersion?: number;
