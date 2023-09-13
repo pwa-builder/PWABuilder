78bd88a0a064146d2cadcb56b91e9760246fefd9
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2023-09-12 17:41:13 -0700

    Remove token banner and remove docs from sidebar

--- docs/src/menuData.ts
+++ docs/src/menuData.ts
@@ -145,12 +145,6 @@ export const parentMenuData: ParentMenu = {
           includeOnHomePage: false
         },
         {
-          pageTitle: "PWABuilder - Microsoft Store Promotion",
-          menuTitle: "Store Promotion",
-          path: "/builder/store-promotion",
-          includeOnHomePage: true
-        },
-        {
           pageTitle: "PWABuilder - FAQ",
           menuTitle: "FAQ",
           path: "/builder/faq",
