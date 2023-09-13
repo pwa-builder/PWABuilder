5c328df7fd097e833701027486ec12551faf8d86
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2023-09-13 13:13:08 -0700

    [CLI][DO NOT MERGE] CLI Initial Release (#3980)
    Initial release for the PWABuilder CLI!
    
    To test, just run `npm run build` and CLI will be available as `pwa`.
    
    The CLI comes with a help command, so see if you can work around it
    without guidance.
    
    ---------
    
    Co-authored-by: Nikola Metulev <nmetulev@users.noreply.github.com>
    Co-authored-by: Justin Willis <jgw9617@gmail.com>

---
+++ apps/cli/src/analytics/usage-analytics.ts
@@ -0,0 +1,108 @@
+import { setup, defaultClient } from 'applicationinsights';
+import { getFlag } from '../flags';
+import * as crypto from 'crypto';
+import * as os from 'os';
+import * as fs from 'fs';
+import { doesFileExist } from '../util/fileUtil';
+
+export interface CreateEventData {
+  template: string,
+  name: string,
+  timeMS: number
+}
+
+export interface StartEventData {}
+
+export interface BuildEventData {
+  timeMS: number
+}
+
+export interface PWABuilderData {
+  user: {
+    id: string
+  }
+}
+
+export function initAnalytics(): void {
+  try {
+    if (getFlag("analytics") === true) {
+      setup("#{ANALYTICS_CODE}#")
+      .setAutoDependencyCorrelation(false)
+      .setAutoCollectRequests(false)
+      .setAutoCollectPerformance(false, false)
+      .setAutoCollectDependencies(false)
+      .setAutoCollectConsole(false)
+      .setUseDiskRetryCaching(false)
+      .start();
+
+      addUserIDtoTelemetry(getUserID());
+    } 
+    
+  }
+  catch (err) {
+    console.error("Error initializing analytics", err);
+  }
+}
+
+export function getAnalyticsClient() {
+  return defaultClient;
+}
+
+// function to trackEvent
+export function trackEvent(name: string, properties: any) {
+  try {
+    if (getFlag("analytics") === true) {
+
+      defaultClient.trackEvent({ 
+        name,  
+        properties
+      });
+    }
+  }
+  catch (err) {
+    console.error("Error tracking event", err);
+    throw new Error(`Error tracking event: ${err}`);
+  }
+}
+
+
+export function trackException(err: Error) {
+  try {
+    if (getFlag("analytics") === true) {
+      defaultClient.trackException({ 
+        exception: err
+      });
+    }
+  }
+  catch (err) {
+    console.error("Error tracking exception", err);
+    throw new Error(`Error tracking exception: ${err}`);
+  }
+}
+
+function getUserID(): string {
+  const pwabuilderDataFilePath: string = os.homedir() + "/.pwabuilder";
+  var userId: string = "";
+
+  if(doesFileExist(pwabuilderDataFilePath)) {
+    const userData: PWABuilderData = JSON.parse(fs.readFileSync(pwabuilderDataFilePath, {encoding: 'utf-8'}));
+    userId = userData.user.id;
+  } else {
+    userId = crypto.randomUUID();
+    const newUserData: PWABuilderData = {
+      user: {
+        id: userId
+      }
+    }
+    fs.writeFileSync(pwabuilderDataFilePath, JSON.stringify(newUserData), {encoding: 'utf-8'});
+  }
+
+  return userId;
+}
+
+function addUserIDtoTelemetry(id: string): void {
+  defaultClient.addTelemetryProcessor((envelope, context) => {
+    envelope["tags"]['ai.user.id'] = id;
+    return true;
+  });
+}
---
+++ apps/cli/src/commands/start.ts
@@ -0,0 +1,60 @@
+import type { Arguments, CommandBuilder } from "yargs";
+import { outputError, isDirectoryTemplate, execSyncWrapper } from "../util/util";
+import { initAnalytics, trackEvent, StartEventData, trackException } from "../analytics/usage-analytics";
+
+const COMMAND_DESCRIPTION_STRING: string = 'Run the PWA Starter on a Vite dev server.';
+const VITEARGS_DESCRIPTION_STRING: string = 'Arguments to pass directly to the Vite start process.';
+
+export const command: string = 'start';
+export const desc: string = COMMAND_DESCRIPTION_STRING;
+
+const USAGE_STRING: string = '$0 start [viteArgs]';
+
+const EXEC_START_NO_ARGS_STRING: string = 'npm run dev-server';
+const EXEC_START_ARGS_STRING: (string) => string = (viteArgs: string) => { return 'npm run dev-task -- ' + viteArgs; }
+
+const INVALID_DIRECTORY_ERROR_STRING: string = `Cannot execute start because the current working directory is not a valid PWA Starter template. 
+Make sure you are executing the start command from a PWA Starter template directory.`;
+
+type StartOptions = {
+  viteArgs: string | undefined;
+}
+
+export const builder: CommandBuilder<StartOptions, StartOptions> = (yargs) =>
+  yargs
+    .options({
+      viteArgs: { type: 'string', description: VITEARGS_DESCRIPTION_STRING}
+    })
+    .usage(USAGE_STRING);
+
+export const handler = (argv: Arguments<StartOptions>): void => {
+  try {
+    handleStartCommand(argv);
+  } catch (error) {
+    trackException(error as Error);
+  }
+};
+
+function handleStartCommand(argv: Arguments<StartOptions>) {
+  const { viteArgs } = argv;
+  if(isDirectoryTemplate()) {
+    execStartCommand(viteArgs);
+  } else {
+    outputError(INVALID_DIRECTORY_ERROR_STRING);
+  }  
+  trackStartEvent();
+}
+
+function execStartCommand(viteArgs: string | undefined) {
+  if(viteArgs) {
+    execSyncWrapper(EXEC_START_ARGS_STRING(viteArgs), false); 
+  } else {
+    execSyncWrapper(EXEC_START_NO_ARGS_STRING, false);
+  }
+}
+
+async function trackStartEvent(): Promise<void> {
+  initAnalytics()
+  const startEventData: StartEventData = {}
+  trackEvent("start", startEventData);
+}
