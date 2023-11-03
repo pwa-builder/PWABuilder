d092521c62fbd45ce46ccd8c352912e9e43112a1
Jaylyn Barbee <jaylyn.1b@gmail.com>
2023-11-03 09:49:31 -0400

    replacing some mistakes i made with replace all

--- apps/pwabuilder/src/script/pages/image-generator.ts
+++ apps/pwabuilder/src/script/pages/image-generator.ts
@@ -280,7 +280,7 @@ export class ImageGenerator extends LitElement {
       return html`<p style="font-size: 16px; color: red;">${this.error}</p>`;
     }
 
-    return null;
+    return html``;
   }
 
   handleInputChange(event: CustomEvent<FileInputDetails>) {
