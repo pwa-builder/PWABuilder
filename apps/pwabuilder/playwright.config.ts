be85d1db2f8e2b7b75d61208790edf591694a332
maiconcarraro <maicon315@gmail.com>
2024-10-08 13:18:37 -0300

    fix: playwright tests

--- apps/pwabuilder/playwright.config.ts
+++ apps/pwabuilder/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.CI ? 'https://preview.pwabuilder.com': 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
