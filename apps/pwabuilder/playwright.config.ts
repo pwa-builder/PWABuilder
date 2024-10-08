a88a43214ffa547bf738f623f78e8308a65cc5b5
maiconcarraro <maicon315@gmail.com>
2024-10-08 13:23:49 -0300

    fix: increase timeout for localhost

--- apps/pwabuilder/playwright.config.ts
+++ apps/pwabuilder/playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: (process.env.CI ? 30 : 50) * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
