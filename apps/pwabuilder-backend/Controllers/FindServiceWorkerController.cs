77b838b3de9f40f66157eaf0be7690e03b95fb54
Karlos Davila <karlos.davila@southworks.com>
2025-06-10 15:41:27 -0300

    add puppeter approach

--- apps/pwabuilder-backend/Controllers/FindServiceWorkerController.cs
+++ apps/pwabuilder-backend/Controllers/FindServiceWorkerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PuppeteerSharp;
 using PWABuilder.Common;
 using PWABuilder.Models;
 
@@ -19,28 +20,61 @@ namespace PWABuilder.Controllers
         {
             var siteUri = new Uri(site);
             using var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", $"{Constant.DESKTOP_USERAGENT} PWABuilderHttpAgent");
-            var response = client.GetAsync(siteUri).Result;
-            if (!response.IsSuccessStatusCode)
+            try
             {
-                return new BadRequestResult();
+                client.DefaultRequestHeaders.Add("User-Agent", $"{Constant.DESKTOP_USERAGENT} PWABuilderHttpAgent");
+                var response = client.GetAsync(siteUri).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new BadRequestResult();
+                }
+
+                var rawHtml = response.Content.ReadAsStringAsync().Result;
+                var link = Finder.getBetween(rawHtml, "rel=\"manifest\"", ">");
+                link = Finder.getBetween(link, "href=\"", "\"");
+                link = link.EndsWith("/") ? link.Remove(link.Length - 1) : link;
+
+                var manifestUri = new Uri(siteUri, link);
+                var manifest = client.GetAsync(manifestUri).Result;
+                if (!manifest.IsSuccessStatusCode)
+                {
+                    return new BadRequestResult();
+                }
+
+                var manifestJson = manifest.Content.ReadFromJsonAsync<object>().Result;
+                return new ManifestResult(manifestJson, manifestUri);
             }
-
-            var rawHtml = response.Content.ReadAsStringAsync().Result;
-            var link = Finder.getBetween(rawHtml, "rel=\"manifest\"", ">");
-            link = Finder.getBetween(link, "href=\"", "\"");
-            link = link.EndsWith("/") ? link.Remove(link.Length - 1) : link;
-
-            var manifestUri = new Uri(siteUri, link);
-            var manifest = client.GetAsync(manifestUri).Result;
-            if (!manifest.IsSuccessStatusCode)
+            catch (Exception ex)
             {
-                return new BadRequestResult();
+                // download the browser executable
+                await new BrowserFetcher().DownloadAsync();
+
+                // browser execution configs
+                var launchOptions = new LaunchOptions
+                {
+                    Headless = true, // = false for testing
+                };
+
+                // open a new page in the controlled browser
+                using var browser = await Puppeteer.LaunchAsync(launchOptions);
+                using var page = await browser.NewPageAsync();
+                
+                // visit the target page
+                await page.GoToAsync(site, 15000, [WaitUntilNavigation.Load]);
+                await page.WaitForNetworkIdleAsync(new() { IdleTime = 1000 });
+                var jsSelectAllManifestLink = @"Array.from(document.querySelectorAll('link[rel*=manifest]')).map(a => a.href);";
+                var urls = await page.EvaluateExpressionAsync<string[]>(jsSelectAllManifestLink);
+
+                var manifestUri = new Uri(siteUri, urls.Last());
+                var manifest = client.GetAsync(manifestUri).Result;
+                if (!manifest.IsSuccessStatusCode)
+                {
+                    return new BadRequestResult();
+                }
+
+                var manifestJson = manifest.Content.ReadFromJsonAsync<object>().Result;
+                return new ManifestResult(manifestJson, manifestUri);
             }
-
-            var manifestJson = manifest.Content.ReadFromJsonAsync<object>().Result;
-
-            return new ManifestResult(manifestJson, manifestUri);
         }
     }
 }
