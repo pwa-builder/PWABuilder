2ceb073f71b6e710cb6a6e0f8ed2d24c2d876cfc
Judah Gabriel Himango <judahgabriel@gmail.com>
2025-06-11 13:51:22 -0700

    Removes the unused /apps/pwabuilder-frontend, as it's moved to /apps/pwabuilder/frontend

--- apps/pwabuilder-frontend/src/script/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-import { dnameInvalidCharacters } from './android-validation';
-
-export const AppNameInputPattern = '[^\\|\\$\\@\\#\\>\\<\\)\\(\\!\\&\\%\\*]+$';
-export const DnameInputPattern = `[^${dnameInvalidCharacters}]+$`;
