0d261fc46e5640af8bd918878e6703c55da10c3f
Karlos Dávila <97609057+KarlosSouthworks@users.noreply.github.com>
2025-06-13 14:58:46 -0300

    find web manifest implementation (#4972)

--- apps/pwabuilder/Program.cs
+++ apps/pwabuilder/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.StaticFiles;
-using Microsoft.PWABuilder.Common;;
+using Microsoft.PWABuilder.Common;
+using PWABuilder.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<ViteEntryPointProvider>();
+builder.Services.AddTransient<PuppeteerService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
