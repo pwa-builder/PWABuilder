214b9cd5872f5f48add681e9d1bedd53aa4082b9
Jose Velarde <josevelarde00@gmail.com>
2025-06-13 11:48:56 -0300

    cleanup

--- apps/pwabuilder-backend/node-scripts/src/custom-audits/develop.md
+++
@@ -1,4 +0,0 @@
-https://github.com/GoogleChrome/lighthouse/blob/main/docs/recipes/custom-gatherer-puppeteer/readme.md
-
-npm i
-npx lighthouse --config-path=custom-config.js https://webboard.app
--- apps/pwabuilder-backend/node-scripts/src/custom-audits/types.ts
+++
@@ -1,8 +0,0 @@
-import * as LH from 'lighthouse/types/lh.js';
-
-interface CustomArtifacts extends LH.Artifacts {
-	ServiceWorkerGatherer: any,
-	OfflineGatherer: any
-}
-
-export default CustomArtifacts;
