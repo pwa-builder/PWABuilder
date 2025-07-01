d25b5ace678def7ac8478688cc4c4f85a4ef094f
Jose Velarde <josevelarde00@gmail.com>
2025-07-01 10:51:54 -0300

    [backend migration] replace azure functions api url paths for relative paths (#4996)
    ## PR Type
    - Other: API migration

--- apps/pwabuilder/Frontend/vite.config.ts
+++ apps/pwabuilder/Frontend/vite.config.ts
@@ -7,7 +7,14 @@ export default defineConfig({
   base: "/",
   server: {
     host: "0.0.0.0",
-    port: 5173
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5777",
+        changeOrigin: true,
+        secure: false
+      }
+    }
   },
   build: {
     sourcemap: true,
