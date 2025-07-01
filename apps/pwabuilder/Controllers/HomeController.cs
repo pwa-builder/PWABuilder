848edc382f2fb66d1ec386ebd47195a4276f63b9
Karlos Dávila <97609057+KarlosSouthworks@users.noreply.github.com>
2025-07-01 15:20:18 -0300

    Fix SPA routing (#4998)

--- apps/pwabuilder/Controllers/HomeController.cs
+++ apps/pwabuilder/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Microsoft.PWABuilder.Controllers;
 
 [Route("[controller]")]
+[Route("{*url:regex(^(?!api|code|assets|.*css$|.*json$|.*js$).*$)}")]
 public class HomeController : Controller
 {
     [HttpGet("")]
