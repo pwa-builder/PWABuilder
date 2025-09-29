a4cd012f25c06a82f80c0fa36c3706c0316a3df9
Judah Gabriel Himango <judahgabriel@gmail.com>
2025-09-29 12:10:37 -0700

    Another TS fix

--- apps/pwabuilder-google-play/services/analytics.ts
+++ apps/pwabuilder-google-play/services/analytics.ts
@@ -1,92 +1,91 @@
-import appInsights, {
-  DistributedTracingModes
-} from 'applicationinsights';
+import * as appInsights from 'applicationinsights';
+import { DistributedTracingModes } from 'applicationinsights';
 
 enum AppInsightsStatus {
-  ENABLED = 1,
-  DISABLED = 0,
-  DEFAULT = -1,
+    ENABLED = 1,
+    DISABLED = 0,
+    DEFAULT = -1,
 }
 
 var appInsightsStatus: AppInsightsStatus = AppInsightsStatus.DEFAULT;
 export function setupAnalytics() {
-  try {
-    appInsights.setup()
-      .setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C)
-      .setAutoDependencyCorrelation(true)
-      .setAutoCollectRequests(false)
-      .setAutoCollectPerformance(false, false)
-      .setAutoCollectExceptions(false)
-      .setAutoCollectDependencies(false)
-      .setAutoCollectConsole(false)
-      .setUseDiskRetryCaching(false)
-      .setSendLiveMetrics(false)
-      .start();
-    appInsightsStatus = AppInsightsStatus.ENABLED;
-    console.log('App insights enabled successfully');
-  } catch (e) {
-    appInsightsStatus = AppInsightsStatus.DISABLED;
-    console.warn("App insights couldn't be enabled", e);
-  }
+    try {
+        appInsights.setup()
+            .setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C)
+            .setAutoDependencyCorrelation(true)
+            .setAutoCollectRequests(false)
+            .setAutoCollectPerformance(false, false)
+            .setAutoCollectExceptions(false)
+            .setAutoCollectDependencies(false)
+            .setAutoCollectConsole(false)
+            .setUseDiskRetryCaching(false)
+            .setSendLiveMetrics(false)
+            .start();
+        appInsightsStatus = AppInsightsStatus.ENABLED;
+        console.log('App insights enabled successfully');
+    } catch (e) {
+        appInsightsStatus = AppInsightsStatus.DISABLED;
+        console.warn("App insights couldn't be enabled", e);
+    }
 }
 
 export function trackEvent(
-  analyticsInfo: AnalyticsInfo,
-  error: string | null,
-  success: boolean
+    analyticsInfo: AnalyticsInfo,
+    error: string | null,
+    success: boolean
 ) {
-  if (appInsightsStatus == AppInsightsStatus.DEFAULT) {
-    setupAnalytics();
-  }
-  if (
-    appInsights.defaultClient == null ||
-    appInsights.defaultClient == undefined ||
-    appInsightsStatus == AppInsightsStatus.DISABLED
-  ) {
-    console.error("App insights no defaultClient");
-    return;
-  }
-  
-  var properties: any = {
-    name: analyticsInfo.name,
-    url: analyticsInfo.url,
-    platformId: analyticsInfo.platformId,
-    platformIdVersion: analyticsInfo.platformIdVersion,
-    referrer: analyticsInfo.referrer,
-  };
-
-  try {
+    if (appInsightsStatus == AppInsightsStatus.DEFAULT) {
+        setupAnalytics();
+    }
     if (
-      analyticsInfo.correlationId != null &&
-      analyticsInfo.correlationId != undefined &&
-      typeof analyticsInfo.correlationId == 'string'
+        appInsights.defaultClient == null ||
+        appInsights.defaultClient == undefined ||
+        appInsightsStatus == AppInsightsStatus.DISABLED
     ) {
-      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.operationId] =
-        analyticsInfo.correlationId;
+        console.error("App insights no defaultClient");
+        return;
     }
-    if (success) {
-      appInsights.defaultClient.trackEvent({
-        name: 'AndroidPackageEvent',
-        properties: properties,
-      });
-    } else {
-      properties.error = error;
-      appInsights.defaultClient.trackEvent({
-        name: 'AndroidPackageFailureEvent',
-        properties: properties,
-      });
+
+    var properties: any = {
+        name: analyticsInfo.name,
+        url: analyticsInfo.url,
+        platformId: analyticsInfo.platformId,
+        platformIdVersion: analyticsInfo.platformIdVersion,
+        referrer: analyticsInfo.referrer,
+    };
+
+    try {
+        if (
+            analyticsInfo.correlationId != null &&
+            analyticsInfo.correlationId != undefined &&
+            typeof analyticsInfo.correlationId == 'string'
+        ) {
+            appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.operationId] =
+                analyticsInfo.correlationId;
+        }
+        if (success) {
+            appInsights.defaultClient.trackEvent({
+                name: 'AndroidPackageEvent',
+                properties: properties,
+            });
+        } else {
+            properties.error = error;
+            appInsights.defaultClient.trackEvent({
+                name: 'AndroidPackageFailureEvent',
+                properties: properties,
+            });
+        }
+    } catch (e) {
+        console.error(e);
     }
-  } catch (e) {
-    console.error(e);
-  }
 }
 
 export type AnalyticsInfo = {
-  url: string;
-  name: string;
-  packageId: string;
-  platformId: string | null;
-  platformIdVersion: string | null;
-  correlationId: string | null;
-  referrer: string | null;
+    url: string;
+    name: string;
+    packageId: string;
+    platformId: string | null;
+    platformIdVersion: string | null;
+    correlationId: string | null;
+    referrer: string | null;
 };
