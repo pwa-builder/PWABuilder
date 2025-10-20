be88798dedca4440c77cfb6d85213023620c2f58
Judah Gabriel Himango <judahgabriel@gmail.com>
2025-10-20 10:42:38 -0700

    Fixing issue with 403 failures on relative image URLs

--- apps/pwabuilder-google-play/services/packageCreator.ts
+++ apps/pwabuilder-google-play/services/packageCreator.ts
@@ -135,8 +135,8 @@ export class PackageCreator {
             bubbleWrapper.addEventListener("progress", e => this.bubblewrapProgress(e))
             return await bubbleWrapper.generateAppPackage();
         } catch (error) {
-            const errorMessage = (error as Error)?.message || '';
-            this.dispatchProgressEvent("Unable to generate app package due to error. Checking if error is 403.", "warn");
+            const errorMessage = (error as Error)?.message || `${error}`;
+            this.dispatchProgressEvent("Unable to generate app package due to error. Checking if error is 403. " + errorMessage, "warn");
             const is403Error =
                 errorMessage.includes('403') ||
                 errorMessage.includes('ECONNREFUSED') ||
@@ -251,8 +251,9 @@ export class PackageCreator {
         for (let prop of absoluteUrlProps) {
             const url = newOptions[prop];
             if (url && typeof url === 'string') {
+                const absoluteUrl = new URL(url, options.webManifestUrl);
                 const safeUrlFetcherEndpoint = 'https://pwabuilder.com/api/images/getsafeimageforanalysis';
-                const safeUrl = `${safeUrlFetcherEndpoint}?imageUrl=${encodeURIComponent(url)}`;
+                const safeUrl = `${safeUrlFetcherEndpoint}?imageUrl=${encodeURIComponent(absoluteUrl.toString())}&analysisId=${encodeURIComponent(options.analysisId || "")}`;
                 (newOptions[prop] as any) = safeUrl;
             }
         }
