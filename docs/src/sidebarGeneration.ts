3914e7ee19ef857ee62fcc5b20e5078c3eb82068
Judah Gabriel Himango <judahgabriel@gmail.com>
2026-01-12 10:49:12 -0800

    Migrates to Docsify 5 preview

--- docs/src/sidebarGeneration.ts
+++ docs/src/sidebarGeneration.ts
@@ -1,87 +1,87 @@
 var fs = require('fs');
-import {Article, ChildMenu, ParentMenu} from './menuInterfaces';
+import { Article, ChildMenu, ParentMenu } from './menuInterfaces';
 import { topLevelNavEntries, headerHTMLString, quickMenuListenerScriptString } from './menuData';
 
 var mainMenu: ParentMenu = {
-  childMenus: []
+    childMenus: []
 }
 
 export function writeMenusFromParentMenu(parentMenu: ParentMenu) {
 
-  for(var childMenu of parentMenu.childMenus) {
-    var condensedChildMenu: ChildMenu = writeChildMenu(childMenu);
-    mainMenu.childMenus = mainMenu.childMenus.concat(condensedChildMenu);
-  }
+    for (var childMenu of parentMenu.childMenus) {
+        var condensedChildMenu: ChildMenu = writeChildMenu(childMenu);
+        mainMenu.childMenus = mainMenu.childMenus.concat(condensedChildMenu);
+    }
 
-  writeMainMenu();
+    writeMainMenu();
 }
 
 function writeChildMenu(child: ChildMenu): ChildMenu {
 
-  const path: string = process.cwd() + child.path + "_sidebar.md";
-  var writeString = headerHTMLString + constructTopLevelNavString(child.header);
-  var condensedChildMenu: ChildMenu = {
-    header: child.header,
-    path: child.path,
-    articles: []
-  }
-
-  if(doesFileExist(path)) {
-    removeFile(path);
-  }
-
-  writeString = writeString + constructHeaderLineString(child);
-  for(var article of child.articles) {
-    writeString = writeString + constructMenuItemLineString(article);
-    if (article.includeOnHomePage) {
-      condensedChildMenu.articles = condensedChildMenu.articles.concat(article);
+    const path: string = process.cwd() + child.path + "_sidebar.md";
+    var writeString = headerHTMLString + constructTopLevelNavString(child.header);
+    var condensedChildMenu: ChildMenu = {
+        header: child.header,
+        path: child.path,
+        articles: []
     }
-  }
 
-  fs.writeFileSync(path, writeString);
+    if (doesFileExist(path)) {
+        removeFile(path);
+    }
 
-  return condensedChildMenu;
+    writeString = writeString + constructHeaderLineString(child);
+    for (var article of child.articles) {
+        writeString = writeString + constructMenuItemLineString(article);
+        if (article.includeOnHomePage) {
+            condensedChildMenu.articles = condensedChildMenu.articles.concat(article);
+        }
+    }
+
+    fs.writeFileSync(path, writeString);
+
+    return condensedChildMenu;
 }
 
 function writeMainMenu() {
-  const path: string = process.cwd() + "/_sidebar.md";
-  var writeString = headerHTMLString + constructTopLevelNavString("Home");
+    const path: string = process.cwd() + "/_sidebar.md";
+    var writeString = headerHTMLString + constructTopLevelNavString("Home");
 
-  for(var childMenu of mainMenu.childMenus) {
-    if(childMenu.articles.length > 0) {
-      writeString = writeString + constructHeaderLineString(childMenu);
-      for(var article of childMenu.articles) {
-        writeString = writeString + constructMenuItemLineString(article);
-      }
-    }  
-  }
+    for (var childMenu of mainMenu.childMenus) {
+        if (childMenu.articles.length > 0) {
+            writeString = writeString + constructHeaderLineString(childMenu);
+            for (var article of childMenu.articles) {
+                writeString = writeString + constructMenuItemLineString(article);
+            }
+        }
+    }
 
-  fs.writeFileSync(path, writeString);
+    fs.writeFileSync(path, writeString);
 }
 
 function constructMenuItemLineString(article: Article): string {
-  return `\n\t- [${article.menuTitle}](${article.path} "${article.pageTitle}")`;
+    return `\n\t- [${article.menuTitle}](${article.path} "${article.pageTitle}")`;
 }
 
 function constructHeaderLineString(childMenu: ChildMenu): string {
-  return `\n\n- <h2>${childMenu.header}</h2> \n`;
+    return `\n\n- <h2>${childMenu.header}</h2> \n`;
 }
 
 function constructTopLevelNavString(activeHeader: string): string {
-  var topLevelNavHTMLString: string = `\n<sl-menu>`;
+    var topLevelNavHTMLString: string = `\n<sl-menu>`;
 
-  for(var entry of topLevelNavEntries) {
-    topLevelNavHTMLString = topLevelNavHTMLString + constructTopLevelNavEntryString(entry, activeHeader);
-  }
+    for (var entry of topLevelNavEntries) {
+        topLevelNavHTMLString = topLevelNavHTMLString + constructTopLevelNavEntryString(entry, activeHeader);
+    }
 
-  topLevelNavHTMLString = topLevelNavHTMLString + `\n</sl-menu>`;
+    topLevelNavHTMLString = topLevelNavHTMLString + `\n</sl-menu>`;
 
-  return topLevelNavHTMLString;
+    return topLevelNavHTMLString;
 }
 
 function constructTopLevelNavEntryString(entry: string[], header: string): string {
-  return `
-  <sl-menu-item onClick="(function (event) {
+    return `
+  <sl-menu-item type="checkbox" onClick="(function (event) {
     location.href = '${entry[1]}';
   })();" ${entry[0] == header ? " checked" : ""}>
     ${entry[0]}
@@ -89,9 +89,9 @@ function constructTopLevelNavEntryString(entry: string[], header: string): strin
 }
 
 function removeFile(path: string): void {
-  fs.rmSync(path, { recursive: true, force: true });
+    fs.rmSync(path, { recursive: true, force: true });
 }
 
 export function doesFileExist(filepath: string): boolean {
-  return fs.existsSync(filepath);
+    return fs.existsSync(filepath);
 }
